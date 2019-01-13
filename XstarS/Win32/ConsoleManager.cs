8c5b7f8be8816e6f5412174141639a54e65f0e65
周建华 <zjh96@live.cn>
2019-01-13 15:36:23 +0800

    Update Win32 related.

--- XstarS/Win32/ConsoleManager.cs
+++ XstarS/Win32/ConsoleManager.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Security;
 
-namespace XstarS
+namespace XstarS.Win32
 {
     /// <summary>
     /// 提供窗口应用程序的控制台窗口管理方法。
@@ -15,17 +15,22 @@ namespace XstarS
     public static class ConsoleManager
     {
         /// <summary>
+        /// kernel32.dll 的名称。
+        /// </summary>
+        private const string Kernel32DllName = "kernel32.dll";
+
+        /// <summary>
         /// 为当前应用程序分配控制台窗口。
         /// </summary>
         /// <returns>是否成功分配控制台窗口。</returns>
-        [DllImport("kernel32.dll")]
+        [DllImport(ConsoleManager.Kernel32DllName)]
         private static extern bool AllocConsole();
 
         /// <summary>
         /// 释放当前已经分配的控制台窗口。
         /// </summary>
         /// <returns>是否成功释放控制台窗口。</returns>
-        [DllImport("kernel32.dll")]
+        [DllImport(ConsoleManager.Kernel32DllName)]
         private static extern bool FreeConsole();
 
         /// <summary>
@@ -33,7 +38,7 @@ namespace XstarS
         /// </summary>
         /// <returns>当前已分配的控制台窗口的句柄。
         /// 若并未分配控制台窗口，则返回 <see cref="IntPtr.Zero"/>。</returns>
-        [DllImport("kernel32.dll")]
+        [DllImport(ConsoleManager.Kernel32DllName)]
         private static extern IntPtr GetConsoleWindow();
 
         /// <summary>
