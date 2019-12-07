e43fd7b3bfcea07c5119b05d97a386f3c94f6ddc
周建华 <zjh96@live.cn>
2019-12-07 21:21:35 +0800

    Add xmlns definition for WPF extensions.

--- XstarS.Windows/Properties/AssemblyInfo.cs
+++ XstarS.Windows/Properties/AssemblyInfo.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Windows.Markup;
 
 // 指示程序元素是否使用公共语言规范进行编译。
 [assembly: CLSCompliant(true)]
+
+// 定义 XAML 命名空间映射。
+[assembly: XmlnsDefinition("https://github.com/x-stars/xswpf", "XstarS.Windows")]
+[assembly: XmlnsDefinition("https://github.com/x-stars/xswpf", "XstarS.Windows.Controls")]
+[assembly: XmlnsDefinition("https://github.com/x-stars/xswpf", "XstarS.Windows.Input")]
+[assembly: XmlnsDefinition("https://github.com/x-stars/xswpf", "XstarS.Windows.Media")]
+[assembly: XmlnsPrefix("https://github.com/x-stars/xswpf", "xswpf")]
