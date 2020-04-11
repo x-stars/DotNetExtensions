edafd81d3166aa3cd8735a69ad555caf8a984d9c
周建华 <zjh96@live.cn>
2020-04-11 10:38:08 +0800

    Fix bugs in WPF WebBrowser error suppressing.

--- XstarS.Windows/Controls/ControlExtensions.cs
+++ XstarS.Windows/Controls/ControlExtensions.cs
@@ -13,27 +13,27 @@ namespace XstarS.Windows.Controls
         /// 尝试设定是否禁止当前 <see cref="WebBrowser"/> 的脚本错误提示。
         /// </summary>
         /// <param name="browser">要设定脚本错误提示的 <see cref="WebBrowser"/> 对象。</param>
-        /// <param name="supresses">指示是否要禁止脚本错误提示。</param>
+        /// <param name="suppresses">指示是否要禁止脚本错误提示。</param>
         /// <returns>若设定成功，则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="browser"/> 为 <see langword="null"/>。</exception>
         [System.Diagnostics.CodeAnalysis.SuppressMessage(
             "Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        public static bool TrySuppressScriptErrors(this WebBrowser browser, bool supresses = true)
+        public static bool TrySuppressScriptErrors(this WebBrowser browser, bool suppresses = true)
         {
             if (browser is null)
             {
                 throw new ArgumentNullException(nameof(browser));
             }
 
-            var _axIWebBrowser2 = typeof(WebBrowser).GetField("_axIWebBrowser2",
+            var axIWebBrowser2 = typeof(WebBrowser).GetProperty("AxIWebBrowser2",
                 BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(browser);
-            if (_axIWebBrowser2 is null) { return false; }
+            if (axIWebBrowser2 is null) { return false; }
 
             try
             {
-                _axIWebBrowser2.GetType().InvokeMember("Silent",
-                    BindingFlags.SetProperty, null, _axIWebBrowser2, new object[] { supresses });
+                axIWebBrowser2.GetType().InvokeMember("Silent",
+                    BindingFlags.SetProperty, null, axIWebBrowser2, new object[] { suppresses });
                 return true;
             }
             catch (Exception) { return false; }
