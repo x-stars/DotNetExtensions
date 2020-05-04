865c1cc8cff66390422e6ab3c541d7aa76197bdb
周建华 <zjh96@live.cn>
2020-05-04 22:45:55 +0800

    Add some object model types.

---
+++ XstarS.ObjectModel/EnumFlagsVectorView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace XstarS.ComponentModel
+{
+    /// <summary>
+    /// 为位域枚举的布尔值向量视图提供抽象基类。
+    /// </summary>
+    /// <typeparam name="TEnum">位域枚举的类型。</typeparam>
+    [Serializable]
+    public abstract class EnumFlagsVectorView<TEnum> : EnumVectorView<TEnum>
+        where TEnum : struct, Enum
+    {
+        /// <summary>
+        /// 初始化 <see cref="EnumFlagsVectorView{TEnum}"/> 类的新实例。
+        /// </summary>
+        protected EnumFlagsVectorView() { }
+
+        /// <summary>
+        /// 获取当前视图表示的枚举值是否包含指定的枚举位域。
+        /// </summary>
+        /// <param name="flagName">要确定的枚举位域的名称。</param>
+        /// <returns>当前视图表示的枚举值是否包含指定的枚举位域值。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="flagName"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="flagName"/> 不为有效的枚举值名称。</exception>
+        protected bool HasFlag(
+            [CallerMemberName] string flagName = null)
+        {
+            return this.Value.HasFlag((TEnum)Enum.Parse(typeof(TEnum), flagName));
+        }
+
+        /// <summary>
+        /// 根据指示设置当前视图表示的枚举值的指定的枚举位域。
+        /// </summary>
+        /// <param name="flagName">要设置的枚举位域的名称。</param>
+        /// <param name="value">指示是添加位域还是移除位域。</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="flagName"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="flagName"/> 不为有效的枚举值名称。</exception>
+        protected void SetFlag(bool value,
+            [CallerMemberName] string flagName = null)
+        {
+            var enumNames = new HashSet<string>(this.Value.ToString().Split(
+                new[] { ", " }, StringSplitOptions.RemoveEmptyEntries));
+            enumNames.IntersectWith(Enum.GetNames(typeof(TEnum)));
+            var flagValue = (TEnum)Enum.Parse(typeof(TEnum), flagName);
+            var flagNames = new HashSet<string>(flagValue.ToString().Split(
+                new[] { ", " }, StringSplitOptions.RemoveEmptyEntries));
+            if (value) { enumNames.UnionWith(flagNames); }
+            else { enumNames.ExceptWith(flagNames); }
+            var enumName = string.Join(", ", enumNames);
+            this.Value = (TEnum)Enum.Parse(typeof(TEnum), enumName);
+            new List<string>(flagNames).ForEach(this.NotifyPropertyChanged);
+        }
+    }
+}
---
+++ XstarS.ObjectModel/EnumVectorView.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace XstarS.ComponentModel
+{
+    /// <summary>
+    /// 为枚举的布尔值向量视图提供抽象基类。
+    /// </summary>
+    /// <typeparam name="TEnum">枚举的类型。</typeparam>
+    [Serializable]
+    public abstract class EnumVectorView<TEnum> : ObservableDataObject
+        where TEnum : struct, Enum
+    {
+        /// <summary>
+        /// 初始化 <see cref="EnumVectorView{TEnum}"/> 类的新实例。
+        /// </summary>
+        protected EnumVectorView() { }
+
+        /// <summary>
+        /// 获取当前视图表示的枚举值。
+        /// </summary>
+        public TEnum Value
+        {
+            get => this.GetProperty<TEnum>();
+            protected set => this.SetProperty(value);
+        }
+
+        /// <summary>
+        /// 获取当前视图表示的枚举值是否为指定的枚举值。
+        /// </summary>
+        /// <param name="enumName">要确定的枚举值的名称。</param>
+        /// <returns>当前视图表示的枚举值是否为指定的枚举值。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="enumName"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="enumName"/> 不为有效的枚举值名称。</exception>
+        protected bool IsEnum(
+            [CallerMemberName] string enumName = null)
+        {
+            var enumValue = (TEnum)Enum.Parse(typeof(TEnum), enumName);
+            return this.Value.ToString() == enumValue.ToString();
+        }
+
+        /// <summary>
+        /// 根据指示设置当前视图表示的枚举值。
+        /// </summary>
+        /// <param name="value">指示是否设置枚举值。</param>
+        /// <param name="enumName">要设置的枚举值的名称。</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="enumName"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="enumName"/> 不为有效的枚举值名称。</exception>
+        protected void SetEnum(bool value,
+            [CallerMemberName] string enumName = null)
+        {
+            if (value)
+            {
+                this.Value = (TEnum)Enum.Parse(typeof(TEnum), enumName);
+                var enumNames = Enum.GetNames(typeof(TEnum));
+                new List<string>(enumNames).ForEach(this.NotifyPropertyChanged);
+            }
+        }
+
+        /// <summary>
+        /// 将此实例的值转换为其等效的字符串表示形式。
+        /// </summary>
+        /// <returns>此实例的值的字符串表示形式。</returns>
+        public override string ToString() => this.Value.ToString();
+    }
+}
