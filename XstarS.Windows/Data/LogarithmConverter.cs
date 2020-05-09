9da7a60ed365a92d73988da4513a0a41cf9086fa
周建华 <zjh96@live.cn>
2020-05-09 23:50:53 +0800

    Change default base of log and exp converters.

--- XstarS.Windows/Data/LogarithmConverter.cs
+++ XstarS.Windows/Data/LogarithmConverter.cs
@@ -21,13 +21,13 @@ namespace XstarS.Windows.Data
         /// </summary>
         /// <param name="value">要转换的 <see cref="IConvertible"/> 数字。</param>
         /// <param name="targetType">绑定目标属性的类型。</param>
-        /// <param name="parameter">表示对数的底的转换器参数，默认为 10。</param>
+        /// <param name="parameter">表示对数的底的转换器参数。默认为常数 <see langword="e"/>。</param>
         /// <param name="culture">要用在转换器中的区域性。</param>
         /// <returns><paramref name="value"/> 的底为 <paramref name="parameter"/> 的对数。</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var number = ((value as IConvertible) ?? 0.0).ToDouble(culture);
-            var @base = ((parameter as IConvertible) ?? 10.0).ToDouble(culture);
+            var @base = ((parameter as IConvertible) ?? Math.E).ToDouble(culture);
             var result = Math.Log(number, @base);
             return ((IConvertible)result).ToType(targetType, culture);
         }
@@ -37,13 +37,13 @@ namespace XstarS.Windows.Data
         /// </summary>
         /// <param name="value">要转换的 <see cref="IConvertible"/> 数字。</param>
         /// <param name="targetType">绑定目标属性的类型。</param>
-        /// <param name="parameter">表示指数的底的转换器参数，默认为 10。</param>
+        /// <param name="parameter">表示指数的底的转换器参数。默认为常数 <see langword="e"/>。</param>
         /// <param name="culture">要用在转换器中的区域性。</param>
         /// <returns><paramref name="value"/> 的底为 <paramref name="parameter"/> 的指数。</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var number = ((value as IConvertible) ?? 0.0).ToDouble(culture);
-            var @base = ((parameter as IConvertible) ?? 10.0).ToDouble(culture);
+            var @base = ((parameter as IConvertible) ?? Math.E).ToDouble(culture);
             var result = Math.Pow(@base, number);
             return ((IConvertible)result).ToType(targetType, culture);
         }
