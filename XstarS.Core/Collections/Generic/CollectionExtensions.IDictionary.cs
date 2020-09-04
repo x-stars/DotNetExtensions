79090dc5c3ef50250d98bb6626f1161ff9ef4c2e
周建华 <zjh96@live.cn>
2020-09-05 01:17:05 +0800

    Add some dictionary extensions.

--- XstarS.Core/Collections/Generic/CollectionExtensions.IDictionary.cs
+++ XstarS.Core/Collections/Generic/CollectionExtensions.IDictionary.cs
@@ -128,5 +128,34 @@ namespace XstarS.Collections.Generic
             }
             return result;
         }
+
+        /// <summary>
+        /// 从指定的的 <see cref="KeyValuePair{TKey, TValue}"/> 的集合创建
+        /// <see cref="Dictionary{TKey, TValue}"/>，并使用指定的 <see cref="IEqualityComparer{T}"/>。
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="KeyValuePair{TKey, TValue}"/> 中的键的类型。</typeparam>
+        /// <typeparam name="TValue"><see cref="KeyValuePair{TKey, TValue}"/> 中的值的类型。</typeparam>
+        /// <param name="pairs">一个 <see cref="KeyValuePair{TKey, TValue}"/> 的集合。</param>
+        /// <param name="comparer">用于比较键的 <see cref="IEqualityComparer{T}"/> 实现。</param>
+        /// <returns>由 <paramref name="pairs"/> 中数据创建的 <see cref="Dictionary{TKey, TValue}"/>。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="pairs"/> 为 <see langword="null"/>。</exception>
+        public static IDictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            this IEnumerable<KeyValuePair<TKey, TValue>> pairs,
+            IEqualityComparer<TKey> comparer = null)
+        {
+            if (pairs is null)
+            {
+                throw new ArgumentNullException(nameof(pairs));
+            }
+
+            comparer = comparer ?? EqualityComparer<TKey>.Default;
+            var dictionary = new Dictionary<TKey, TValue>(comparer);
+            foreach (var pair in pairs)
+            {
+                dictionary[pair.Key] = pair.Value;
+            }
+            return dictionary;
+        }
     }
 }
