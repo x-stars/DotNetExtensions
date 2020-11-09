c7795dbecce474a2bca80d4d9ac9e63c80c5632b
周建华 <zjh96@live.cn>
2020-11-09 22:51:56 +0800

    Add dotnet fsi reference script.

---
+++ Invoke-DotNetFsi.ps1
@@ -0,0 +1,6 @@
+$Projects = Get-Item 'XstarS.*' | ForEach-Object { $_.Name }
+$Configuration, $Framework = 'Release', 'netcoreapp3.1'
+$Pattern = "$PSScriptRoot\{0}\bin\$Configuration\$Framework\{0}.dll"
+$Assemblies = $Projects | ForEach-Object { $Pattern -f $_ } | Where-Object { Test-Path $_ }
+$References = $Assemblies | ForEach-Object { "--reference:""$_""" }
+dotnet fsi $References
