711dc344a995869be95d71586d5f6efe9d0b04fc
周建华 <zjh96@live.cn>
2020-11-19 22:18:43 +0800

    Add array initialization extension methods.

--- XstarS.Core/ArrayExtensions.cs
+++ XstarS.Core/ArrayExtensions.cs
@@ -233,18 +233,18 @@ namespace XstarS
 
         /// <summary>
         /// 返回一个长度等于当前 32 位有符号整数的指定类型的数组，
-        /// 数组的每个元素由 <see cref="Converter{TInput, TOutput}"/> 转换索引得到。
+        /// 数组的每个元素由 <see cref="Func{T, TResult}"/> 转换索引得到。
         /// </summary>
         /// <typeparam name="T">要创建的数组的元素的类型。</typeparam>
         /// <param name="length">要创建的数组的长度。</param>
         /// <param name="indexMap">用于将索引转换到数组元素的转换器。</param>
-        /// <returns>长度为 <paramref name="length"/> 的数组，
+        /// <returns>一个长度为 <paramref name="length"/> 的数组，
         /// 其中的每个元素由 <paramref name="indexMap"/> 转换索引得到。</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="indexMap"/> 为 <see langword="null"/>。</exception>
         /// <exception cref="ArgumentOutOfRangeException">
         /// <paramref name="length"/> 小于 0。</exception>
-        public static T[] InitializeArray<T>(this int length, Converter<int, T> indexMap)
+        public static T[] InitializeArray<T>(this int length, Func<int, T> indexMap)
         {
             if (length < 0)
             {
@@ -264,8 +264,95 @@ namespace XstarS
         }
 
         /// <summary>
+        /// 返回一个大小等于当前 32 位有符号整数二元组的指定类型的二维数组，
+        /// 数组的每个元素由 <see cref="Func{T1, T2, TResult}"/> 转换索引得到。
+        /// </summary>
+        /// <typeparam name="T">要创建的数组的元素的类型。</typeparam>
+        /// <param name="lengths">要创建的二维数组的大小。</param>
+        /// <param name="indicesMap">用于将索引转换到数组元素的转换器。</param>
+        /// <returns>一个大小为 <paramref name="lengths"/> 的数组，
+        /// 其中的每个元素由 <paramref name="indicesMap"/> 转换索引得到。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="indicesMap"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="lengths"/> 中有元素小于 0。</exception>
+        public static T[,] InitializeArray<T>(
+            this (int, int) lengths, Func<int, int, T> indicesMap)
+        {
+            if (lengths.Item1 < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lengths));
+            }
+            if (lengths.Item2 < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lengths));
+            }
+            if (indicesMap is null)
+            {
+                throw new ArgumentNullException(nameof(indicesMap));
+            }
+
+            var result = new T[lengths.Item1, lengths.Item2];
+            for (int i = 0; i < lengths.Item1; i++)
+            {
+                for (int j = 0; j < lengths.Item2; j++)
+                {
+                    result[i, j] = indicesMap(i, j);
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 返回一个大小等于当前 32 位有符号整数三元组的指定类型的三维数组，
+        /// 数组的每个元素由 <see cref="Func{T1, T2, T3, TResult}"/> 转换索引得到。
+        /// </summary>
+        /// <typeparam name="T">要创建的数组的元素的类型。</typeparam>
+        /// <param name="lengths">要创建的三维数组的大小。</param>
+        /// <param name="indicesMap">用于将索引转换到数组元素的转换器。</param>
+        /// <returns>一个大小为 <paramref name="lengths"/> 的数组，
+        /// 其中的每个元素由 <paramref name="indicesMap"/> 转换索引得到。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="indicesMap"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="lengths"/> 中有元素小于 0。</exception>
+        public static T[,,] InitializeArray<T>(
+            this (int, int, int) lengths, Func<int, int, int, T> indicesMap)
+        {
+            if (lengths.Item1 < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lengths));
+            }
+            if (lengths.Item2 < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lengths));
+            }
+            if (lengths.Item3 < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lengths));
+            }
+            if (indicesMap is null)
+            {
+                throw new ArgumentNullException(nameof(indicesMap));
+            }
+
+            var result = new T[lengths.Item1, lengths.Item2, lengths.Item3];
+            for (int i = 0; i < lengths.Item1; i++)
+            {
+                for (int j = 0; j < lengths.Item2; j++)
+                {
+                    for (int k = 0; k < lengths.Item3; k++)
+                    {
+                        result[i, j, k] = indicesMap(i, j, k);
+                    }
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
         /// 返回一个大小等于当前 32 位有符号整数数组的指定类型的多维数组，
-        /// 数组的每个元素由 <see cref="Converter{TInput, TOutput}"/> 转换索引得到。
+        /// 数组的每个元素由 <see cref="Func{T, TResult}"/> 转换索引得到。
         /// </summary>
         /// <typeparam name="T">要创建的数组的元素的类型。</typeparam>
         /// <param name="lengths">要创建的数组在各维度的长度。</param>
@@ -278,7 +365,7 @@ namespace XstarS
         /// <paramref name="lengths"/> 中没有任何元素。</exception>
         /// <exception cref="ArgumentOutOfRangeException">
         /// <paramref name="lengths"/> 中包含小于 0 的整数。</exception>
-        public static Array InitializeArray<T>(this int[] lengths, Converter<int[], T> indicesMap)
+        public static Array InitializeArray<T>(this int[] lengths, Func<int[], T> indicesMap)
         {
             if (lengths is null)
             {
