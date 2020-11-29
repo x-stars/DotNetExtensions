cbeec6d9989738ccd5a407291d9ff16d65c5716f
周建华 <zjh96@live.cn>
2020-11-29 21:01:12 +0800

    Add dictionary cast to generic extension methods.

--- XstarS.Core/Collections/CollectionExtensions.cs
+++ XstarS.Core/Collections/CollectionExtensions.cs
@@ -11,6 +11,32 @@ namespace XstarS.Collections
     public static class CollectionExtensions
     {
         /// <summary>
+        /// 将当前 <see cref="IDictionary"/> 中的键值对强制转换为指定类型。
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="IDictionary"/> 中键要转换到的类型。</typeparam>
+        /// <typeparam name="TValue"><see cref="IDictionary"/> 中值要转换到的类型。</typeparam>
+        /// <param name="dictionary">要强制转换键值对类型的 <see cref="IDictionary"/> 对象。</param>
+        /// <returns>将 <paramref name="dictionary"/> 中的键值对强制转换类型后得到的序列。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="dictionary"/> 为 <see langword="null"/>。</exception>
+        /// <exception cref="InvalidCastException">
+        /// <paramref name="dictionary"/> 中包含无法转换为 <typeparamref name="TKey"/>
+        /// 类型的键或无法转换为 <typeparamref name="TValue"/> 类型的值。</exception>
+        public static IEnumerable<KeyValuePair<TKey, TValue>> Cast<TKey, TValue>(
+            this IDictionary dictionary)
+        {
+            if (dictionary is null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                yield return new KeyValuePair<TKey, TValue>((TKey)entry.Key, (TValue)entry.Value);
+            }
+        }
+
+        /// <summary>
         /// 确定当前 <see cref="IEnumerable"/> 与指定 <see cref="IEnumerable"/> 的元素是否对应相等。
         /// </summary>
         /// <param name="enumerable">要进行相等比较的 <see cref="IEnumerable"/> 对象。</param>
