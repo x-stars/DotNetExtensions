813457ffd092d34b66dbacd06507432ed9d6c914
周建华 <zjh96@live.cn>
2020-11-30 00:57:53 +0800

    Add interface for object text format.

---
+++ XstarS.Core/Text/ITextFormatter.cs
@@ -0,0 +1,29 @@
+﻿namespace XstarS.Text
+{
+    /// <summary>
+    /// 提供将对象格式化为字符串的方法。
+    /// </summary>
+    public interface ITextFormatter
+    {
+        /// <summary>
+        /// 将指定对象格式化为字符串。
+        /// </summary>
+        /// <param name="value">要格式化的对象。</param>
+        /// <returns><paramref name="value"/> 的格式化字符串。</returns>
+        string Format(object value);
+    }
+
+    /// <summary>
+    /// 提供将指定类型的对象格式化为字符串的方法。
+    /// </summary>
+    /// <typeparam name="T">要格式化为字符串的对象的类型。</typeparam>
+    public interface ITextFormatter<T>
+    {
+        /// <summary>
+        /// 将指定对象格式化为字符串。
+        /// </summary>
+        /// <param name="value">要格式化的对象。</param>
+        /// <returns><paramref name="value"/> 的格式化字符串。</returns>
+        string Format(T value);
+    }
+}
---
+++ XstarS.Core/Text/TextFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace XstarS.Text
+{
+    /// <summary>
+    /// 为 <see cref="ITextFormatter{T}"/> 提供抽象基类。
+    /// </summary>
+    [Serializable]
+    public abstract class TextFormatter<T> : ITextFormatter, ITextFormatter<T>
+    {
+        /// <summary>
+        /// 将指定对象格式化为字符串。
+        /// </summary>
+        /// <param name="value">要格式化的对象。</param>
+        /// <returns><paramref name="value"/> 的格式化字符串。</returns>
+        public abstract string Format(T value);
+
+        /// <summary>
+        /// 将指定对象格式化为字符串。
+        /// </summary>
+        /// <param name="value">要格式化的对象。</param>
+        /// <returns><paramref name="value"/> 的格式化字符串。</returns>
+        /// <exception cref="InvalidCastException">
+        /// <paramref name="value"/> 无法转换强到 <typeparamref name="T"/> 类型。</exception>
+        string ITextFormatter.Format(object value) => this.Format((T)value);
+    }
+}
