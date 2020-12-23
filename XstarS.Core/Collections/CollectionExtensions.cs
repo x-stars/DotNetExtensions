b89e8898626efa236893a74226ba3a6bed214ed9
周建华 <zjh96@live.cn>
2020-12-24 00:59:45 +0800

    Add as-generic extension methods for collections.

--- XstarS.Core/Collections/CollectionExtensions.cs
+++ XstarS.Core/Collections/CollectionExtensions.cs
@@ -10,6 +10,54 @@ namespace XstarS.Collections
     public static class CollectionExtensions
     {
         /// <summary>
+        /// 将当前 <see cref="IList"/> 强制转换为指定元素类型的泛型集合。
+        /// </summary>
+        /// <typeparam name="T"><see cref="IList"/> 中的元素要转换为的类型。</typeparam>
+        /// <param name="collection">要转换为指定元素类型的 <see cref="IList"/> 对象。</param>
+        /// <returns><paramref name="collection"/> 的指定元素类型的泛型包装。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="collection"/> 为 <see langword="null"/>。</exception>
+        public static GenericCollection<T> AsGeneric<T>(this IList collection) =>
+            new GenericCollection<T>(collection);
+
+        /// <summary>
+        /// 将当前 <see cref="IDictionary"/> 强制转换为指定键值类型的泛型键值对集合。
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="IDictionary"/> 中的键要转换为的类型。</typeparam>
+        /// <typeparam name="TValue"><see cref="IDictionary"/> 中的值要转换为的类型。</typeparam>
+        /// <param name="dictionary">要转换为指定键值类型的 <see cref="IDictionary"/> 对象。</param>
+        /// <returns><paramref name="dictionary"/> 的指定键值类型的泛型包装。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="dictionary"/> 为 <see langword="null"/>。</exception>
+        public static GenericDictionary<TKey, TValue> AsGeneric<TKey, TValue>(
+            this IDictionary dictionary) =>
+            new GenericDictionary<TKey, TValue>(dictionary);
+
+        /// <summary>
+        /// 将当前 <see cref="IEnumerator"/> 强制转换为指定元素类型的泛型集合的枚举数。
+        /// </summary>
+        /// <typeparam name="T"><see cref="IEnumerator"/> 中的元素要转换为的类型。</typeparam>
+        /// <param name="enumerator">要转换为指定元素类型的 <see cref="IEnumerator"/> 对象。</param>
+        /// <returns><paramref name="enumerator"/> 的指定元素类型的泛型包装。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="enumerator"/> 为 <see langword="null"/>。</exception>
+        public static GenericEnumerator<T> AsGeneric<T>(this IEnumerator enumerator) =>
+            new GenericEnumerator<T>(enumerator);
+
+        /// <summary>
+        /// 将当前 <see cref="IDictionaryEnumerator"/> 强制转换为指定键值类型的泛型字典的枚举数。
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="IDictionaryEnumerator"/> 中的键要转换为的类型。</typeparam>
+        /// <typeparam name="TValue"><see cref="IDictionaryEnumerator"/> 中的值要转换为的类型。</typeparam>
+        /// <param name="enumerator">要强制转换键值类型的 <see cref="IDictionaryEnumerator"/> 对象。</param>
+        /// <returns><paramref name="enumerator"/> 的指定键值类型的泛型包装。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="enumerator"/> 为 <see langword="null"/>。</exception>
+        public static GenericDictionaryEnumerator<TKey, TValue> AsGeneric<TKey, TValue>(
+            this IDictionaryEnumerator enumerator) =>
+            new GenericDictionaryEnumerator<TKey, TValue>(enumerator);
+
+        /// <summary>
         /// 将当前 <see cref="IDictionary"/> 中的键值对强制转换为指定类型。
         /// </summary>
         /// <typeparam name="TKey"><see cref="IDictionary"/> 中键要转换到的类型。</typeparam>
@@ -18,9 +66,6 @@ namespace XstarS.Collections
         /// <returns>将 <paramref name="dictionary"/> 中的键值对强制转换类型后得到的序列。</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="dictionary"/> 为 <see langword="null"/>。</exception>
-        /// <exception cref="InvalidCastException">
-        /// <paramref name="dictionary"/> 中包含无法转换为 <typeparamref name="TKey"/>
-        /// 类型的键或无法转换为 <typeparamref name="TValue"/> 类型的值。</exception>
         public static IEnumerable<KeyValuePair<TKey, TValue>> Cast<TKey, TValue>(
             this IDictionary dictionary)
         {
