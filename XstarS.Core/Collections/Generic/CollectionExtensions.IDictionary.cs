7d3f1ae3180d34c47b6a659d160724f8f9b0df4e
周建华 <zjh96@live.cn>
2020-12-26 00:09:13 +0800

    Split generic dictionary extension methods.

--- XstarS.Core/Collections/Generic/CollectionExtensions.IDictionary.cs
+++ XstarS.Core/Collections/Generic/CollectionExtensions.IDictionary.cs
@@ -227,37 +227,5 @@ namespace XstarS.Collections.Generic
             comparer = comparer ?? EqualityComparer<TKey>.Default;
             return new ConcurrentDictionary<TKey, TValue>(dictionary, comparer);
         }
-
-        /// <summary>
-        /// 从指定的的 <see cref="KeyValuePair{TKey, TValue}"/> 的集合创建
-        /// <see cref="Dictionary{TKey, TValue}"/>，并使用指定的 <see cref="IEqualityComparer{T}"/>。
-        /// </summary>
-        /// <typeparam name="TKey"><see cref="KeyValuePair{TKey, TValue}"/> 中的键的类型。</typeparam>
-        /// <typeparam name="TValue"><see cref="KeyValuePair{TKey, TValue}"/> 中的值的类型。</typeparam>
-        /// <param name="pairs">要创建 <see cref="Dictionary{TKey, TValue}"/> 的
-        /// <see cref="KeyValuePair{TKey, TValue}"/> 的集合。</param>
-        /// <param name="comparer">用于比较键的 <see cref="IEqualityComparer{T}"/> 实现。</param>
-        /// <returns>由 <paramref name="pairs"/> 中数据创建的 <see cref="Dictionary{TKey, TValue}"/>。</returns>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="pairs"/> 为 <see langword="null"/>。</exception>
-        /// <exception cref="ArgumentException">
-        /// <see cref="Dictionary{TKey, TValue}"/> 中已存在具有相同键的元素。</exception>
-        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
-            this IEnumerable<KeyValuePair<TKey, TValue>> pairs,
-            IEqualityComparer<TKey> comparer = null)
-        {
-            if (pairs is null)
-            {
-                throw new ArgumentNullException(nameof(pairs));
-            }
-
-            comparer = comparer ?? EqualityComparer<TKey>.Default;
-            var dictionary = new Dictionary<TKey, TValue>(comparer);
-            foreach (var pair in pairs)
-            {
-                dictionary.Add(pair.Key, pair.Value);
-            }
-            return dictionary;
-        }
     }
 }
---
+++ XstarS.Core/Linq/LinqTypeExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XstarS.Linq
+{
+    /// <summary>
+    /// 提供 LINQ 相关类型的扩展方法。
+    /// </summary>
+    public static class LinqTypeExtensions
+    {
+        /// <summary>
+        /// 从指定的的 <see cref="IGrouping{TKey, TElement}"/> 的集合创建 <see cref="Dictionary{TKey, TValue}"/>。
+        /// </summary>
+        /// <typeparam name="TKey"><see cref="IGrouping{TKey, TElement}"/> 中的键的类型。</typeparam>
+        /// <typeparam name="TElement"><see cref="IGrouping{TKey, TElement}"/> 中的元素的类型。</typeparam>
+        /// <param name="groups">要创建 <see cref="Dictionary{TKey, TValue}"/> 的
+        /// <see cref="IGrouping{TKey, TElement}"/> 的集合。</param>
+        /// <param name="comparer">用于比较键的 <see cref="IEqualityComparer{T}"/> 实现。</param>
+        /// <returns>由 <paramref name="groups"/> 中数据创建的 <see cref="Dictionary{TKey, TValue}"/>。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="groups"/>
+        /// 或 <paramref name="groups"/> 中的元素为 <see langword="null"/>。</exception>
+        /// <exception cref="ArgumentException">
+        /// <see cref="Dictionary{TKey, TValue}"/> 中已存在具有相同键的元素。</exception>
+        public static Dictionary<TKey, IEnumerable<TElement>> ToDictionary<TKey, TElement>(
+            this IEnumerable<IGrouping<TKey, TElement>> groups,
+            IEqualityComparer<TKey> comparer = null)
+        {
+            if (groups is null)
+            {
+                throw new ArgumentNullException(nameof(groups));
+            }
+
+            comparer = comparer ?? EqualityComparer<TKey>.Default;
+
+            var result = new Dictionary<TKey, IEnumerable<TElement>>(comparer);
+            foreach (var group in groups)
+            {
+                if (group is null)
+                {
+                    throw new ArgumentNullException(nameof(group));
+                }
+
+                result.Add(group.Key, group);
+            }
+            return result;
+        }
+    }
+}
