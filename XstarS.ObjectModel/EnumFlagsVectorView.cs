963e06f4a7e33d61b3dad53f55fe3df0d4cedf26
周建华 <zjh96@live.cn>
2021-02-14 21:14:12 +0800

    Extract base class for enum vector views.

--- XstarS.ObjectModel/EnumFlagsVectorView.cs
+++ XstarS.ObjectModel/EnumFlagsVectorView.cs
@@ -8,7 +8,7 @@ namespace XstarS.ComponentModel
     /// </summary>
     /// <typeparam name="TEnum">位域枚举的类型。</typeparam>
     [Serializable]
-    public class EnumFlagsVectorView<TEnum> : ObservableDataObject
+    public class EnumFlagsVectorView<TEnum> : EnumViewBase<TEnum>
         where TEnum : struct, Enum
     {
         /// <summary>
@@ -25,23 +25,7 @@ namespace XstarS.ComponentModel
         public bool this[TEnum flagValue]
         {
             get => this.Value.HasFlag(flagValue);
-            set
-            {
-                var iValue = ((IConvertible)this.Value).ToUInt64(null);
-                var iFlag = ((IConvertible)flagValue).ToUInt64(null);
-                if (value) { iValue |= iFlag; } else { iValue &= ~iFlag; }
-                this.Value = (TEnum)Enum.ToObject(typeof(TEnum), iValue);
-            }
-        }
-
-        /// <summary>
-        /// 获取或设置当前视图表示的枚举值。
-        /// </summary>
-        /// <returns>当前视图表示的枚举值。</returns>
-        public TEnum Value
-        {
-            get => this.GetProperty<TEnum>();
-            set => this.SetProperty(value);
+            set => this.SetFlag(flagValue, value);
         }
 
         /// <summary>
@@ -62,11 +46,11 @@ namespace XstarS.ComponentModel
         /// <summary>
         /// 根据指示设置当前视图表示的枚举值的指定的枚举位域。
         /// </summary>
-        /// <param name="flagName">要设置的枚举位域的名称。</param>
         /// <param name="value">指示是添加位域还是移除位域。</param>
+        /// <param name="flagName">要设置的枚举位域的名称。</param>
         /// <exception cref="ArgumentException">
         /// <paramref name="flagName"/> 不为有效的枚举值名称。</exception>
-        protected virtual void SetFlag(bool value,
+        protected void SetFlag(bool value,
             [CallerMemberName] string flagName = null)
         {
             flagName = flagName ?? string.Empty;
@@ -74,21 +58,16 @@ namespace XstarS.ComponentModel
         }
 
         /// <summary>
-        /// 设置指定属性的值。
+        /// 根据指示设置当前视图表示的枚举值的指定的枚举位域。
         /// </summary>
-        /// <typeparam name="T">属性的类型。</typeparam>
-        /// <param name="value">属性的新值。</param>
-        /// <param name="propertyName">要设置值的属性的名称。</param>
-        protected override void SetProperty<T>(T value,
-            [CallerMemberName] string propertyName = null)
+        /// <param name="flagValue">要设置的枚举位域。</param>
+        /// <param name="value">指示是否设置枚举值。</param>
+        protected virtual void SetFlag(TEnum flagValue, bool value)
         {
-            base.SetProperty(value, propertyName);
-            if (propertyName == nameof(this.Value))
-            {
-                var flagNames = Enum.GetNames(typeof(TEnum));
-                Array.ForEach(flagNames, this.NotifyPropertyChanged);
-                this.NotifyPropertyChanged(ObservableDataObject.IndexerName);
-            }
+            var iValue = ((IConvertible)this.Value).ToUInt64(null);
+            var iFlag = ((IConvertible)flagValue).ToUInt64(null);
+            if (value) { iValue |= iFlag; } else { iValue &= ~iFlag; }
+            this.Value = (TEnum)Enum.ToObject(typeof(TEnum), iValue);
         }
     }
 }
---
+++ XstarS.ObjectModel/EnumViewBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace XstarS.ComponentModel
+{
+    /// <summary>
+    /// 提供枚举的视图的抽象基类。
+    /// </summary>
+    /// <typeparam name="TEnum">枚举的类型。</typeparam>
+    [Serializable]
+    public abstract class EnumViewBase<TEnum> : ObservableDataObject
+        where TEnum : struct, Enum
+    {
+        /// <summary>
+        /// 初始化 <see cref="EnumVectorView{TEnum}"/> 类的新实例。
+        /// </summary>
+        protected EnumViewBase() { }
+
+        /// <summary>
+        /// 获取或设置当前视图表示的枚举值。
+        /// </summary>
+        /// <returns>当前视图表示的枚举值。</returns>
+        public TEnum Value
+        {
+            get => this.GetProperty<TEnum>();
+            set => this.SetEnumValue(value);
+        }
+
+        /// <summary>
+        /// 设置当前视图表示的枚举值。
+        /// </summary>
+        /// <param name="enumValue">要设置的枚举值。</param>
+        protected virtual void SetEnumValue(TEnum enumValue)
+        {
+            var thisValue = this.Value;
+            this.SetProperty(enumValue, nameof(this.Value));
+            var valueChanged = !object.Equals(thisValue, enumValue);
+            if (valueChanged) { this.NotifyEnumPropertiesChanged(); }
+        }
+
+        /// <summary>
+        /// 通知所有枚举值对应的属性的值已更改。
+        /// </summary>
+        protected void NotifyEnumPropertiesChanged()
+        {
+            var enumNames = Enum.GetNames(typeof(TEnum));
+            Array.ForEach(enumNames, this.NotifyPropertyChanged);
+            this.NotifyPropertyChanged(ObservableDataObject.IndexerName);
+        }
+
+        /// <summary>
+        /// 将当前视图表示的枚举值转换为其等效的字符串表示形式。
+        /// </summary>
+        /// <returns>当前视图表示的枚举值的字符串表示形式。</returns>
+        public override string ToString() => this.Value.ToString();
+    }
+}
