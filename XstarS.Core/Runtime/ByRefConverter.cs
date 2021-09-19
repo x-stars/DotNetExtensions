ac03a030481e36c64202e147d304741ee2d8df3b
周建华 <zjh96@live.cn>
2021-09-20 01:43:03 +0800

    Redesign byref to pointer converter.

---
+++ XstarS.Core/Runtime/ByRefConverter.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using XstarS.Reflection.Emit;
+
+namespace XstarS.Runtime
+{
+    /// <summary>
+    /// 提供转换引用传递 <see langword="ref"/> 类型的方法。
+    /// </summary>
+    public abstract unsafe class ByRefConverter
+    {
+        /// <summary>
+        /// 表示提供 <see cref="ByRefConverter"/> 方法实现的延迟初始化对象。
+        /// </summary>
+        private static readonly Lazy<ByRefConverter> LazyImplementation =
+            new Lazy<ByRefConverter>(ByRefConverter.CreateImplementation);
+
+        /// <summary>
+        /// 初始化 <see cref="ByRefConverter"/> 类型的新实例。
+        /// 仅用于内部实现，用户不应继承此类型。
+        /// </summary>
+        [Obsolete("This constructor is for internal usages only.", error: true)]
+        protected ByRefConverter() { }
+
+        /// <summary>
+        /// 表示提供 <see cref="ByRefConverter"/> 方法实现的对象。
+        /// </summary>
+        private static ByRefConverter Implementation =>
+            ByRefConverter.LazyImplementation.Value;
+
+        /// <summary>
+        /// 将指定的 <see cref="IntPtr"/> 转换为等效的引用传递 <see langword="ref"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
+        /// <param name="pointer">引用值的 <see cref="IntPtr"/>。</param>
+        /// <returns><paramref name="pointer"/> 对应的引用转递 <see langword="ref"/>。</returns>
+        public static ref T FromIntPtr<T>(IntPtr pointer) =>
+            ref ByRefConverter.Implementation.ConvertFromIntPtr<T>(pointer);
+
+        /// <summary>
+        /// 将指定的未知类型的指针转换为等效的引用传递 <see langword="ref"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
+        /// <param name="pointer">引用值的 <see cref="IntPtr"/>。</param>
+        /// <returns><paramref name="pointer"/> 对应的引用转递 <see langword="ref"/>。</returns>
+        [CLSCompliant(false)]
+        public static ref T FromPointer<T>(void* pointer) =>
+            ref ByRefConverter.FromIntPtr<T>((IntPtr)pointer);
+
+        /// <summary>
+        /// 将指定的引用传递 <see langword="ref"/> 转换为等效的 <see cref="IntPtr"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用传递的值的类型。</typeparam>
+        /// <param name="reference">要进行转换的引用传递 <see langword="ref"/>。</param>
+        /// <returns><paramref name="reference"/> 转换得到的 <see cref="IntPtr"/>。</returns>
+        public static IntPtr ToIntPtr<T>(ref T reference) =>
+            ByRefConverter.Implementation.ConvertToIntPtr<T>(ref reference);
+
+        /// <summary>
+        /// 将指定的引用传递 <see langword="ref"/> 转换为未知类型的指针。
+        /// </summary>
+        /// <typeparam name="T">按引用传递的值的类型。</typeparam>
+        /// <param name="reference">要进行转换的引用传递 <see langword="ref"/>。</param>
+        /// <returns><paramref name="reference"/> 转换得到的未知类型的指针。</returns>
+        [CLSCompliant(false)]
+        public static void* ToPointer<T>(ref T reference) =>
+            (void*)ByRefConverter.ToIntPtr<T>(ref reference);
+
+        /// <summary>
+        /// 将指定的引用传递 <see langword="ref"/> 转换为指定的另一类型的引用传递。
+        /// </summary>
+        /// <typeparam name="TInput">按引用传递的值的类型。</typeparam>
+        /// <typeparam name="TOutput">要转换到的按引用传递的值的类型。</typeparam>
+        /// <param name="value">要进行类型转换的引用传递 <see langword="ref"/>。</param>
+        /// <returns><paramref name="value"/> 转换到另一类型的引用传递。</returns>
+        public static ref TOutput ChangeType<TInput, TOutput>(ref TInput value) =>
+            ref ByRefConverter.Implementation.ChangeRefType<TInput, TOutput>(ref value);
+
+        /// <summary>
+        /// 将指定的 <see cref="IntPtr"/> 转换为等效的引用传递 <see langword="ref"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
+        /// <param name="pointer">引用值的 <see cref="IntPtr"/>。</param>
+        /// <returns><paramref name="pointer"/> 对应的引用转递 <see langword="ref"/>。</returns>
+        public abstract ref T ConvertFromIntPtr<T>(IntPtr pointer);
+
+        /// <summary>
+        /// 将指定的引用传递 <see langword="ref"/> 转换为等效的 <see cref="IntPtr"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用传递的值的类型。</typeparam>
+        /// <param name="reference">要进行转换的引用传递 <see langword="ref"/>。</param>
+        /// <returns><paramref name="reference"/> 转换得到的 <see cref="IntPtr"/>。</returns>
+        public abstract IntPtr ConvertToIntPtr<T>(ref T reference);
+
+        /// <summary>
+        /// 将指定的引用传递 <see langword="ref"/> 转换为指定的另一类型的引用传递。
+        /// </summary>
+        /// <typeparam name="TInput">按引用传递的值的类型。</typeparam>
+        /// <typeparam name="TOutput">要转换到的按引用传递的值的类型。</typeparam>
+        /// <param name="value">要进行类型转换的引用传递 <see langword="ref"/>。</param>
+        /// <returns><paramref name="value"/> 转换到另一类型的引用传递。</returns>
+        public abstract ref TOutput ChangeRefType<TInput, TOutput>(ref TInput value);
+
+        /// <summary>
+        /// 创建提供 <see cref="ByRefConverter"/> 方法实现的对象。
+        /// </summary>
+        /// <returns>提供 <see cref="ByRefConverter"/> 方法实现的对象。</returns>
+        private static ByRefConverter CreateImplementation()
+        {
+            var typeName = typeof(ByRefConverter).ToString();
+            var assembly = AssemblyBuilder.DefineDynamicAssembly(
+                new AssemblyName(typeName), AssemblyBuilderAccess.Run);
+            var module = assembly.DefineDynamicModule($"{typeName}.dll");
+            var baseType = typeof(ByRefConverter);
+            var type = module.DefineType(typeName, TypeAttributes.Public |
+                TypeAttributes.Sealed | TypeAttributes.BeforeFieldInit, baseType);
+
+            var constructor = type.DefineDefaultConstructor(MethodAttributes.Public);
+            var baseMethods = baseType.GetMethods(
+                BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            foreach (var baseMethod in baseMethods)
+            {
+                var method = type.DefineMethodOverride(baseMethod);
+                var il = method.GetILGenerator();
+                il.Emit(OpCodes.Ldarg_1);
+                il.Emit(OpCodes.Ret);
+            }
+
+            return (ByRefConverter)Activator.CreateInstance(type.CreateTypeInfo());
+        }
+    }
+}
--- XstarS.DynamicProxy/ProxyServices.cs
+++ XstarS.DynamicProxy/ProxyServices.cs
@@ -9,34 +9,21 @@ namespace XstarS.Reflection
     public static class ProxyServices
     {
         /// <summary>
+        /// 将当前 <see cref="IntPtr"/> 转换为等效的引用传递 <see langword="ref"/>。
+        /// </summary>
+        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
+        /// <param name="pointer">引用值的 <see cref="IntPtr"/>。</param>
+        /// <returns><paramref name="pointer"/> 对应的引用转递 <see langword="ref"/>。</returns>
+        public static ref T ToByRef<T>(this IntPtr pointer) =>
+            ref ByRefConverter.FromIntPtr<T>(pointer);
+
+        /// <summary>
         /// 将指定的引用传递 <see langword="ref"/> 转换为等效的 <see cref="IntPtr"/>。
         /// </summary>
-        /// <typeparam name="T">引用传递 <see langword="ref"/> 的类型。</typeparam>
+        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
         /// <param name="value">按引用转递的值。</param>
         /// <returns><paramref name="value"/> 的引用转换得到的 <see cref="IntPtr"/>。</returns>
-        public static IntPtr RefAsIntPtr<T>(ref T value) =>
-            IntPtrByRef.RefAsIntPtr(ref value);
-
-        /// <summary>
-        /// 获取当前 <see cref="IntPtr"/> 表示的引用传递 <see langword="ref"/> 所引用的值。
-        /// </summary>
-        /// <typeparam name="T">引用传递 <see langword="ref"/> 的类型。</typeparam>
-        /// <param name="reference">
-        /// 以 <see cref="IntPtr"/> 表示的引用传递 <see langword="ref"/>。</param>
-        /// <returns><paramref name="reference"/>
-        /// 表示的引用传递 <see langword="ref"/> 所引用的值。</returns>
-        public static T GetRefValue<T>(this IntPtr reference) =>
-            IntPtrByRef.GetRefValue<T>(reference);
-
-        /// <summary>
-        /// 设置当前 <see cref="IntPtr"/> 表示的引用传递 <see langword="ref"/> 所引用的值。
-        /// </summary>
-        /// <typeparam name="T">引用传递 <see langword="ref"/> 的类型。</typeparam>
-        /// <param name="reference">
-        /// 以 <see cref="IntPtr"/> 表示的引用传递 <see langword="ref"/>。</param>
-        /// <param name="value">要设置为 <see cref="IntPtr"/>
-        /// 表示的引用传递 <see langword="ref"/> 所引用的值。</param>
-        public static void SetRefValue<T>(this IntPtr reference, T value) =>
-            IntPtrByRef.SetRefValue(reference, value);
+        public static IntPtr ToIntPtr<T>(ref T value) =>
+            ByRefConverter.ToIntPtr(ref value);
     }
 }
