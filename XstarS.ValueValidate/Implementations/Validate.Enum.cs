d4077476af06447e70a6f0b6de1c55a8778ae2d6
周建华 <zjh96@live.cn>
2021-10-24 11:12:26 +0800

    Optimize performance when validate enums.

--- XstarS.ValueValidate/Implementations/Validate.Enum.cs
+++ XstarS.ValueValidate/Implementations/Validate.Enum.cs
@@ -1,10 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace XstarS
 {
     partial class Validate
     {
+        /// <summary>
+        /// 提供用于验证枚举值的信息。
+        /// </summary>
+        /// <typeparam name="T">枚举的类型。</typeparam>
+        private static class EnumInfo<T> where T : struct, Enum
+        {
+            /// <summary>
+            /// 表示当前枚举类型的所有值的集合。
+            /// </summary>
+            private static readonly HashSet<T> Values =
+                new HashSet<T>((T[])Enum.GetValues(typeof(T)));
+
+            /// <summary>
+            /// 表示当前位域枚举类型的所有位域的组合。
+            /// </summary>
+            private static readonly ulong AllFlags =
+                ((T[])Enum.GetValues(typeof(T))).Aggregate(
+                    0UL, (last, value) => last | Convert.ToUInt64(value));
+
+            /// <summary>
+            /// 获取当前枚举类型是否为位域枚举类型。
+            /// </summary>
+            /// <returns>若当前枚举类型带有 <see cref="FlagsAttribute"/> 特性，
+            /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+            internal static bool IsFlags { get; } =
+                typeof(T).GetCustomAttributes(false).Any(attr => attr is FlagsAttribute);
+
+            /// <summary>
+            /// 确定当前枚举类型是否包含指定的枚举值。
+            /// </summary>
+            /// <param name="value">要确定是否包含的枚举值。</param>
+            /// <returns>若当前枚举类型包含 <paramref name="value"/> 表示的枚举值，
+            /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+            internal static bool ContainsValue(T value) => EnumInfo<T>.Values.Contains(value);
+
+            /// <summary>
+            /// 确定当前位域枚举类型是否包含指定的位域组合值。
+            /// </summary>
+            /// <param name="flag">要确定是否包含的位域组合值。</param>
+            /// <returns>若当前枚举类型包含 <paramref name="flag"/> 表示的位域组合值，
+            /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+            internal static bool ContainsFlag(T flag) =>
+                (EnumInfo<T>.AllFlags | Convert.ToUInt64(flag)) == EnumInfo<T>.AllFlags;
+        }
+
         /// <summary>
         /// 验证枚举值是否是一个有效的枚举值。
         /// </summary>
@@ -18,22 +64,16 @@ namespace XstarS
             this IValueInfo<T> valueInfo, string message = null)
             where T : struct, Enum
         {
-            if (typeof(T).GetCustomAttributes(false).Any(attr => attr is FlagsAttribute))
+            if (EnumInfo<T>.IsFlags)
             {
-                var values = Enum.GetValues(typeof(T));
-                long allValues = 0L;
-                foreach (var value in values)
-                {
-                    allValues |= Convert.ToInt64(value);
-                }
-                if ((allValues | Convert.ToInt64(valueInfo.Value)) != allValues)
+                if (!EnumInfo<T>.ContainsFlag(valueInfo.Value))
                 {
                     ThrowHelper.ThrowArgumentOutOfRangeException(valueInfo.Name, valueInfo.Value, message);
                 }
             }
             else
             {
-                if (!Enum.GetValues(typeof(T)).Cast<T>().Contains(valueInfo.Value))
+                if (!EnumInfo<T>.ContainsValue(valueInfo.Value))
                 {
                     ThrowHelper.ThrowArgumentOutOfRangeException(valueInfo.Name, valueInfo.Value, message);
                 }
