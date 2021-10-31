6d0ecee832d3e5d5ab92c1c132bbae9181e30354
周建华 <zjh96@live.cn>
2021-10-31 11:21:58 +0800

    Add Base64 convert extension method.

--- XstarS.Core/ArrayExtensions.cs
+++ XstarS.Core/ArrayExtensions.cs
@@ -357,9 +357,18 @@ namespace XstarS
         }
 
         /// <summary>
+        /// 将当前字节数组转换为其用 Base64 数字编码的等效字符串表示形式。
+        /// </summary>
+        /// <param name="bytes">要进行转换的字节数组。</param>
+        /// <returns>当前字节数组的 Base64 数字编码的等效字符串表示形式。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="bytes"/> 为 <see langword="null"/>。</exception>
+        public static string ToBase64String(this byte[] bytes) => Convert.ToBase64String(bytes);
+
+        /// <summary>
         /// 将当前字节数组转换为十六进制数字的等效字符串表示形式。
         /// </summary>
-        /// <param name="bytes">要转换为字符串的字节数组。</param>
+        /// <param name="bytes">要进行转换的字节数组。</param>
         /// <returns>当前字节数组的十六进制数字的等效字符串表示形式。</returns>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="bytes"/> 为 <see langword="null"/>。</exception>
