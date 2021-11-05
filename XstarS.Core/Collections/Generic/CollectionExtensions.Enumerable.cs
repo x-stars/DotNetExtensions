62c94690eec1f2739c3be6371afbefd22b8baa78
周建华 <zjh96@live.cn>
2021-11-05 23:06:27 +0800

    Remove duplicated item counting method.

--- XstarS.Core/Collections/Generic/CollectionExtensions.Enumerable.cs
+++ XstarS.Core/Collections/Generic/CollectionExtensions.Enumerable.cs
@@ -66,38 +66,6 @@ namespace XstarS.Collections.Generic
         }
 
         /// <summary>
-        /// 获取 <see cref="IEnumerable{T}"/> 对象中满足指定条件的对象的数量。
-        /// </summary>
-        /// <typeparam name="T"><see cref="IEnumerable{T}"/> 中的元素的类型。</typeparam>
-        /// <param name="enumerable">要进行计数的 <see cref="IEnumerable{T}"/> 对象。</param>
-        /// <param name="match"><see cref="IEnumerable{T}"/> 中参与计数的对象应满足的条件。</param>
-        /// <returns><paramref name="enumerable"/> 中满足条件
-        /// <paramref name="match"/> 的对象的数量。</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="enumerable"/>
-        /// 或 <paramref name="match"/> 为 <see langword="null"/>。</exception>
-        public static int CountOf<T>(this IEnumerable<T> enumerable, Predicate<T> match)
-        {
-            if (enumerable is null)
-            {
-                throw new ArgumentNullException(nameof(enumerable));
-            }
-            if (match is null)
-            {
-                throw new ArgumentNullException(nameof(match));
-            }
-
-            int count = 0;
-            foreach (var item in enumerable)
-            {
-                if (match.Invoke(item))
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
-
-        /// <summary>
         /// 对 <see cref="IEnumerable{T}"/> 对象中的每个元素执行指定操作。
         /// </summary>
         /// <typeparam name="T"><see cref="IEnumerable{T}"/> 中的元素的类型。</typeparam>
