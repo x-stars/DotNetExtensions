3483a88a8fa6d9387614321b1ca200dbe7360f92
周建华 <zjh96@live.cn>
2021-11-21 19:24:20 +0800

    Fix DispatchProxy types from nullable branch.

--- XstarS.DispatchProxy/InvocationHandler.cs
+++ XstarS.DispatchProxy/InvocationHandler.cs
@@ -3,11 +3,12 @@
 namespace XstarS.Reflection
 {
     /// <summary>
-    /// 表示方法调用时所用的代理委托。
+    /// 表示调用代理类型方法时所用的代理委托。
     /// </summary>
-    /// <param name="instance">方法的实例参数。</param>
-    /// <param name="method">当前调用的方法。</param>
-    /// <param name="arguments">方法的参数列表。</param>
-    /// <returns>方法的返回值。若无返回值，应为 <see langword="null"/>。</returns>
+    /// <param name="instance">代理类型包装的代理对象。</param>
+    /// <param name="method">调用方调用的方法。</param>
+    /// <param name="arguments">调用方传递给方法的参数。</param>
+    /// <returns>要返回给调用方的对象。或者对于
+    /// <see langword="void"/> 方法，应为 <see langword="null"/>。</returns>
     public delegate object InvocationHandler(object instance, MethodInfo method, object[] arguments);
 }
