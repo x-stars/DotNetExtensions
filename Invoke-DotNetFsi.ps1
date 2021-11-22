fd42b17509f8c98a3313a4200f6680cf821bdac9
周建华 <zjh96@live.cn>
2021-11-22 21:15:52 +0800

    Update reference passing in dotnet fsi script.

--- Invoke-DotNetFsi.ps1
+++ Invoke-DotNetFsi.ps1
@@ -1,6 +1,6 @@
 $Projects = Get-Item 'XstarS.*' | ForEach-Object { $_.Name }
 $Configuration, $Framework = 'Release', 'net6.0*'
 $Pattern = "$PSScriptRoot\{0}\bin\$Configuration\$Framework\{0}.dll"
-$Assemblies = $Projects | ForEach-Object { $Pattern -f $_ } | Where-Object { Test-Path $_ }
-$References = $Assemblies | Get-Item | ForEach-Object { "--reference:""$($_.FullName)""" }
+$Assemblies = $Projects | ForEach-Object { $Pattern -f $_ }
+$References = $Assemblies | Get-Item | ForEach-Object { '-r', $_.FullName }
 dotnet fsi --langversion:latest @References
