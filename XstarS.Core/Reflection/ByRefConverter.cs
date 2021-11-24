573414e2f21f55d520af6d42426a0f50240a4372
周建华 <zjh96@live.cn>
2021-11-24 23:33:17 +0800

    Optimize method to reference boxed value type.

--- XstarS.Core/Reflection/ByRefConverter.cs
+++ XstarS.Core/Reflection/ByRefConverter.cs
@@ -9,31 +9,30 @@ namespace XstarS.Reflection
     public static unsafe class ByRefConverter
     {
         /// <summary>
-        /// 将指定的 <see cref="IntPtr"/> 转换为等效的引用传递 <see langword="ref"/>。
+        /// 将指定的 <see cref="IntPtr"/> 表示的地址转换为等效的 <typeparamref name="T"/> 类型的值的引用。
         /// </summary>
-        /// <typeparam name="T">按引用转递的值的类型。</typeparam>
-        /// <param name="pointer">引用值的 <see cref="IntPtr"/>。</param>
-        /// <returns><paramref name="pointer"/> 对应的引用转递 <see langword="ref"/>。</returns>
+        /// <typeparam name="T">按引用传递的值的类型。</typeparam>
+        /// <param name="pointer">指向 <typeparamref name="T"/> 类型的值的 <see cref="IntPtr"/>。</param>
+        /// <returns>对 <paramref name="pointer"/> 指向的 <typeparamref name="T"/> 类型的值的引用。</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ref T ToByRef<T>(nint pointer) => ref Unsafe.AsRef<T>((void*)pointer);
 
         /// <summary>
-        /// 将指定的引用传递 <see langword="ref"/> 转换为等效的 <see cref="IntPtr"/>。
+        /// 将指定的 <typeparamref name="T"/> 类型的值的引用转换为等效的 <see cref="IntPtr"/> 表示的地址。
         /// </summary>
         /// <typeparam name="T">按引用传递的值的类型。</typeparam>
-        /// <param name="reference">要进行转换的引用传递 <see langword="ref"/>。</param>
-        /// <returns><paramref name="reference"/> 转换得到的 <see cref="IntPtr"/>。</returns>
+        /// <param name="reference">要获取地址的 <typeparamref name="T"/> 类型的值的引用。</param>
+        /// <returns>指向 <paramref name="reference"/> 引用的值并以 <see cref="IntPtr"/> 表示的地址。</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static nint ToIntPtr<T>(ref T reference) => (nint)Unsafe.AsPointer(ref reference);
 
         /// <summary>
-        /// 获取指定装箱值类型中已装箱值的引用传递 <see langword="ref"/>。
+        /// 获取指定装箱值类型中已装箱的 <typeparamref name="T"/> 类型的值的引用。
         /// </summary>
         /// <typeparam name="T">已装箱的值的类型。</typeparam>
         /// <param name="box">已装箱为 <see cref="object"/> 的值类型。</param>
-        /// <returns><paramref name="box"/> 中已装箱值的引用转递 <see langword="ref"/>。</returns>
+        /// <returns>对 <paramref name="box"/> 中已装箱的 <typeparamref name="T"/> 类型的值的引用。</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ref T RefBoxed<T>(object box) where T : struct =>
-            ref Unsafe.AsRef<T>(*(nint**)Unsafe.AsPointer(ref box) + 1);
+        public static ref T RefBoxed<T>(object box) where T : struct => ref Unsafe.As<StrongBox<T>>(box).Value;
     }
 }
