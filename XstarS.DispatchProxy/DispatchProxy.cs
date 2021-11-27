278cced4404f71ecaf1281937f92ef7d27b15fd2
周建华 <zjh96@live.cn>
2021-11-27 21:46:51 +0800

    Add extra create methods for DispatchProxy.

--- XstarS.DispatchProxy/DispatchProxy.cs
+++ XstarS.DispatchProxy/DispatchProxy.cs
@@ -7,7 +7,7 @@ namespace XstarS.Reflection
     /// <summary>
     /// 为代理类型 <see cref="DispatchProxy"/> 提供基于代理委托的实现。
     /// </summary>
-    /// <typeparam name="TInterface">要代理的类型，应为接口。</typeparam>
+    /// <typeparam name="TInterface">代理应实现的接口类型。</typeparam>
     public class DispatchProxy<TInterface> : DispatchProxy where TInterface : class
     {
         /// <summary>
@@ -21,16 +21,19 @@ namespace XstarS.Reflection
         protected InvocationHandler Handler;
 
         /// <summary>
-        /// 初始化 <see cref="DispatchProxy{TInterface}"/> 类的新实例。
-        /// 用于内部实现，应使用 <see langword="protected"/> 构造函数实现继承。
+        /// 初始化 <see cref="DispatchProxy{TInterface}"/> 类的新实例。用于内部实现，不应直接使用。
         /// </summary>
+        /// <remarks>
+        /// <para>使用 <see cref="DispatchProxy{TInterface}(object)"/> 构造函数实现继承。</para>
+        /// <para>使用 <see cref="DispatchProxy{TInterface}.Create(TInterface, InvocationHandler)"/>
+        /// 方法创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。</para>
+        /// </remarks>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        [Obsolete("This constructor is for internal usages only.", error: true)]
+        [Obsolete("For internal usages only, use Create method insted.", error: true)]
         public DispatchProxy() : base() { }
 
         /// <summary>
-        /// 初始化 <see cref="DispatchProxy{TInterface}"/> 类的新实例。
-        /// 应使用此构造函数实现继承，而非 <see langword="public"/> 构造函数。
+        /// 初始化 <see cref="DispatchProxy{TInterface}"/> 类的新实例。应使用此构造函数实现继承。
         /// </summary>
         /// <param name="unused">不使用此参数，应为 <see langword="null"/>。</param>
         protected DispatchProxy(object unused) : base() { }
@@ -53,33 +56,27 @@ namespace XstarS.Reflection
         /// <summary>
         /// 不支持此方法，总是抛出 <see cref="NotSupportedException"/> 异常。
         /// </summary>
+        /// <remarks>
+        /// 使用 <see cref="DispatchProxy{TInterface}.Create(TInterface, InvocationHandler)"/>
+        /// 方法创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。
+        /// </remarks>
         /// <typeparam name="T">代理应实现的接口。</typeparam>
         /// <typeparam name="TProxy">要用于代理类的基类。</typeparam>
         /// <returns>总是抛出 <see cref="NotSupportedException"/> 异常。</returns>
         /// <exception cref="NotSupportedException">总是抛出此异常。</exception>
         [EditorBrowsable(EditorBrowsableState.Never)]
-        [Obsolete("Not supported, use non-generic method instead.", error: true)]
-        public static new T Create<T, TProxy>() where TProxy : DispatchProxy =>
-            throw new NotSupportedException();
-
-        /// <summary>
-        /// 使用指定的代理对象和默认的代理委托创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。
-        /// </summary>
-        /// <param name="instance">要代理的 <typeparamref name="TInterface"/> 类型的对象。</param>
-        /// <returns>以指定的参数初始化的 <see cref="DispatchProxy{TInterface}"/> 类的实例。</returns>
-        /// <exception cref="ArgumentNullException">
-        /// <paramref name="instance"/> 为 <see langword="null"/>。</exception>
-        public static TInterface Create(TInterface instance)
+        [Obsolete("Not supported, use non-generic Create method instead.", error: true)]
+        public static new T Create<T, TProxy>() where TProxy : DispatchProxy
         {
-            return DispatchProxy<TInterface>.Create(instance, DispatchProxyServices.DefaultHandler);
+            throw new NotSupportedException();
         }
 
         /// <summary>
         /// 使用指定的代理对象和代理委托创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。
         /// </summary>
         /// <param name="instance">要代理的 <typeparamref name="TInterface"/> 类型的对象。</param>
-        /// <param name="handler">方法调用时所用的 <see cref="Handler"/> 代理委托。</param>
-        /// <returns>以指定的参数初始化的 <see cref="DispatchProxy{TInterface}"/> 类的实例。</returns>
+        /// <param name="handler">方法调用时所用的 <see cref="InvocationHandler"/> 代理委托。</param>
+        /// <returns>实现 <typeparamref name="TInterface"/> 接口的代理类型的对象。</returns>
         /// <exception cref="ArgumentNullException"><paramref name="instance"/>
         /// 或 <paramref name="handler"/> 为 <see langword="null"/>。</exception>
         public static TInterface Create(TInterface instance, InvocationHandler handler)
--- XstarS.DispatchProxy/DispatchProxyServices.cs
+++ XstarS.DispatchProxy/DispatchProxyServices.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 namespace XstarS.Reflection
 {
     /// <summary>
-    /// 提供 <see cref="DispatchProxy{TInterface}"/> 相关的服务方法。
+    /// 提供代理类型 <see cref="DispatchProxy{TInterface}"/> 相关的服务方法。
     /// </summary>
     public static class DispatchProxyServices
     {
@@ -19,8 +19,50 @@ namespace XstarS.Reflection
         /// <summary>
         /// 表示方法对应的动态调用委托。
         /// </summary>
-        private static readonly ConcurrentDictionary<MethodInfo, Func<object, object[], object>> DynamicDelegates =
+        private static readonly ConcurrentDictionary<MethodInfo, Func<object, object[], object>> MethodDelegates =
             new ConcurrentDictionary<MethodInfo, Func<object, object[], object>>();
+
+        /// <summary>
+        /// 表示类型对应的代理创建方法的委托。
+        /// </summary>
+        private static readonly ConcurrentDictionary<Type, Func<object, object[], object>> ProxyCreateDelegates =
+            new ConcurrentDictionary<Type, Func<object, object[], object>>();
+
+        /// <summary>
+        /// 使用指定的代理对象和代理委托创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。
+        /// </summary>
+        /// <typeparam name="TInterface">代理应实现的接口类型。</typeparam>
+        /// <param name="instance">要代理的 <typeparamref name="TInterface"/> 类型的对象。</param>
+        /// <param name="handler">方法调用时所用的 <see cref="InvocationHandler"/> 代理委托。</param>
+        /// <returns>实现 <typeparamref name="TInterface"/> 接口的代理类型的对象。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/>
+        /// 或 <paramref name="handler"/> 为 <see langword="null"/>。</exception>
+        public static TInterface CreateProxy<TInterface>(TInterface instance, InvocationHandler handler = null)
+            where TInterface : class
+        {
+            handler ??= DispatchProxyServices.DefaultHandler;
+            return DispatchProxy<TInterface>.Create(instance, handler);
+        }
+
+        /// <summary>
+        /// 使用指定的代理对象和代理委托创建 <see cref="DispatchProxy{TInterface}"/> 类的实例。
+        /// </summary>
+        /// <param name="interfaceType">代理应实现的接口类型。</param>
+        /// <param name="instance">要代理的 <paramref name="interfaceType"/> 类型的对象。</param>
+        /// <param name="handler">方法调用时所用的 <see cref="InvocationHandler"/> 代理委托。</param>
+        /// <returns>实现 <paramref name="interfaceType"/> 接口的代理类型的对象。</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/>
+        /// 或 <paramref name="handler"/> 为 <see langword="null"/>。</exception>
+        public static object CreateProxy(Type interfaceType, object instance, InvocationHandler handler = null)
+        {
+            if (interfaceType is null) { throw new ArgumentNullException(nameof(interfaceType)); }
+            handler ??= DispatchProxyServices.DefaultHandler;
+            var createDelegate = DispatchProxyServices.ProxyCreateDelegates.GetOrAdd(interfaceType,
+                newInterfaceType => typeof(DispatchProxy<>).MakeGenericType(newInterfaceType).GetMethod(
+                    nameof(DispatchProxy<object>.Create), new[] { newInterfaceType, typeof(InvocationHandler) }
+                ).CreateDynamicDelegate());
+            return createDelegate.Invoke(null, new[] { instance, handler });
+        }
 
         /// <summary>
         /// 获取当前方法的动态调用委托。
@@ -36,7 +78,7 @@ namespace XstarS.Reflection
                 throw new ArgumentNullException(nameof(method));
             }
 
-            return DispatchProxyServices.DynamicDelegates.GetOrAdd(method,
+            return DispatchProxyServices.MethodDelegates.GetOrAdd(method,
                 newMethod => newMethod.CreateDynamicDelegate());
         }
     }
