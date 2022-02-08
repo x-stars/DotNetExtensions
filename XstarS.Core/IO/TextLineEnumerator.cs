f2255d099ac33fd8f681487e9a91a6cb9f2cc454
天南十字星 <zjh96@live.cn>
2022-02-08 23:54:46 +0800

    Fix async dispose method of enumerator.

--- XstarS.Core/IO/TextLineEnumerator.cs
+++ XstarS.Core/IO/TextLineEnumerator.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
 using System.Threading;
 using System.Threading.Tasks;
+#endif
 
 namespace XstarS.IO
 {
     /// <summary>
     /// 为 <see cref="TextReader"/> 提供按行迭代的公开枚举数。
     /// </summary>
-    internal sealed partial class TextLineEnumerator : IEnumerable<string>, IEnumerator<string>
+    internal sealed class TextLineEnumerator : IEnumerable<string>, IEnumerator<string>
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        , IAsyncEnumerable<string>, IAsyncEnumerator<string>
+#endif
     {
         /// <summary>
         /// 表示要按行进行迭代的文本读取器。
@@ -25,7 +30,7 @@ namespace XstarS.IO
         /// <summary>
         /// 表示当前迭代读取到的文本行。
         /// </summary>
-        private string? CurrentLine;
+        private volatile string? CurrentLine;
 
         /// <summary>
         /// 以指定的文本读取器初始化 <see cref="TextLineEnumerator"/> 类的新实例，
@@ -35,7 +40,7 @@ namespace XstarS.IO
         /// <param name="disposing">指定是否在迭代完成后释放输入的文本读取器。</param>
         /// <exception cref="ArgumentNullException">
         /// <paramref name="reader"/> 为 <see langword="null"/>。</exception>
-        public TextLineEnumerator(TextReader reader, bool disposing = false)
+        internal TextLineEnumerator(TextReader reader, bool disposing = false)
         {
             this.Reader = reader ??
                 throw new ArgumentNullException(nameof(reader));
@@ -51,40 +56,40 @@ namespace XstarS.IO
         /// <inheritdoc/>
         public void Dispose() { if (this.Disposing) { this.Reader.Dispose(); } }
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <inheritdoc/>
+        public ValueTask DisposeAsync()
+        {
+            try { this.Dispose(); return default(ValueTask); }
+            catch (Exception e) { return new ValueTask(Task.FromException(e)); }
+        }
+#endif
+
         /// <inheritdoc/>
         public IEnumerator<string> GetEnumerator() => this;
 
         /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator() => this;
 
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
         /// <inheritdoc/>
-        public bool MoveNext() => !((this.CurrentLine = this.Reader.ReadLine()) is null);
+        public IAsyncEnumerator<string> GetAsyncEnumerator(
+            CancellationToken cancellationToken = default) => this;
+#endif
+
+        /// <inheritdoc/>
+        public bool MoveNext() => (this.CurrentLine = this.Reader.ReadLine()) != null;
+
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <inheritdoc/>
+        public async ValueTask<bool> MoveNextAsync() =>
+            (this.CurrentLine = await this.Reader.ReadLineAsync()) != null;
+#endif
 
         /// <summary>
         /// 不支持此操作，永远抛出 <see cref="NotSupportedException"/> 异常。
         /// </summary>
         /// <exception cref="NotSupportedException">永远抛出此异常。</exception>
-        public void Reset() => throw new NotSupportedException();
+        void IEnumerator.Reset() => throw new NotSupportedException();
     }
-
-#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-    partial class TextLineEnumerator : IAsyncEnumerable<string>, IAsyncEnumerator<string>
-    {
-        /// <summary>
-        /// 表示已经完成 <see cref="ValueTask"/> 任务。
-        /// </summary>
-        private static readonly ValueTask CompletedTask = new ValueTask(Task.CompletedTask);
-
-        /// <inheritdoc/>
-        public ValueTask DisposeAsync() { this.Dispose(); return CompletedTask; }
-
-        /// <inheritdoc/>
-        public IAsyncEnumerator<string> GetAsyncEnumerator(
-            CancellationToken cancellationToken = default) => this;
-
-        /// <inheritdoc/>
-        public async ValueTask<bool> MoveNextAsync() =>
-            !((this.CurrentLine = await this.Reader.ReadLineAsync()) is null);
-    }
-#endif
 }
