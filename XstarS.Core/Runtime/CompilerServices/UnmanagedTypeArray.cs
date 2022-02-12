6ab2c9e3a42ec790b5d3e991d2ad40b58b5ca35c
天南十字星 <zjh96@live.cn>
2022-02-12 11:02:31 +0800

    Add span binary equality extension methods.

--- XstarS.Core/Runtime/CompilerServices/UnmanagedTypeArray.cs
+++ XstarS.Core/Runtime/CompilerServices/UnmanagedTypeArray.cs
@@ -1,17 +1,21 @@
 ﻿namespace XstarS.Runtime.CompilerServices
 {
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+    using System;
+#endif
+
     /// <summary>
     /// 提供非托管类型的数组相关的帮助方法。
     /// </summary>
     public static unsafe class UnmanagedTypeArray
     {
         /// <summary>
-        /// 确定当前非托管类型的数组与指定非托管类型的数组是否二进制相等。
+        /// 确定当前非托管类型的数组与指定非托管类型的数组的值是否二进制相等。
         /// </summary>
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要进行二进制相等比较的非托管类型的数组。</param>
         /// <param name="other">要与当前值进行比较的非托管类型的数组。</param>
-        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 二进制相等，
+        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
         public static bool BinaryEquals<T>(this T[]? array, T[]? other) where T : unmanaged
         {
@@ -27,12 +31,12 @@
         }
 
         /// <summary>
-        /// 确定当前非托管类型的二维数组与指定非托管类型的二维数组是否二进制相等。
+        /// 确定当前非托管类型的二维数组与指定非托管类型的二维数组的值是否二进制相等。
         /// </summary>
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要进行二进制相等比较的非托管类型的二维数组。</param>
         /// <param name="other">要与当前值进行比较的非托管类型的二维数组。</param>
-        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 二进制相等，
+        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
         public static bool BinaryEquals<T>(this T[,]? array, T[,]? other) where T : unmanaged
         {
@@ -48,12 +52,12 @@
         }
 
         /// <summary>
-        /// 确定当前非托管类型的三维数组与指定非托管类型的三维数组是否二进制相等。
+        /// 确定当前非托管类型的三维数组与指定非托管类型的三维数组的值是否二进制相等。
         /// </summary>
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要进行二进制相等比较的非托管类型的三维数组。</param>
         /// <param name="other">要与当前值进行比较的非托管类型的三维数组。</param>
-        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 二进制相等，
+        /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
         public static bool BinaryEquals<T>(this T[,,]? array, T[,,]? other) where T : unmanaged
         {
@@ -67,6 +71,50 @@
                 return BinaryEqualityComparer.Equals(pArray, pOther, size);
             }
         }
+
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>
+        /// 确定当前非托管类型的跨度与指定非托管类型的只读跨度的值是否二进制相等。
+        /// </summary>
+        /// <typeparam name="T">非托管类型的跨度中的元素的类型。</typeparam>
+        /// <param name="span">要进行二进制相等比较的非托管类型的跨度。</param>
+        /// <param name="other">要与当前跨度进行比较的非托管类型的只读跨度。</param>
+        /// <returns>若 <paramref name="span"/> 与 <paramref name="other"/> 的值二进制相等，
+        /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        public static bool BinaryEquals<T>(this Span<T> span, ReadOnlySpan<T> other)
+            where T : unmanaged
+        {
+            if (span == other) { return true; }
+            if (span.Length != other.Length) { return false; }
+
+            fixed (T* pSpan = span, pOther = other)
+            {
+                var size = sizeof(T) * span.Length;
+                return BinaryEqualityComparer.Equals(pSpan, pOther, size);
+            }
+        }
+
+        /// <summary>
+        /// 确定当前非托管类型的只读跨度与指定非托管类型的只读跨度的值是否二进制相等。
+        /// </summary>
+        /// <typeparam name="T">非托管类型的跨度中的元素的类型。</typeparam>
+        /// <param name="span">要进行二进制相等比较的非托管类型的只读跨度。</param>
+        /// <param name="other">要与当前跨度进行比较的非托管类型的只读跨度。</param>
+        /// <returns>若 <paramref name="span"/> 与 <paramref name="other"/> 的值二进制相等，
+        /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        public static bool BinaryEquals<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> other)
+            where T : unmanaged
+        {
+            if (span == other) { return true; }
+            if (span.Length != other.Length) { return false; }
+
+            fixed (T* pSpan = span, pOther = other)
+            {
+                var size = sizeof(T) * span.Length;
+                return BinaryEqualityComparer.Equals(pSpan, pOther, size);
+            }
+        }
+#endif
 
         /// <summary>
         /// 获取当前非托管类型的数组基于二进制值的哈希代码。
