7f8fb31d15491401ca0de549e4563c7b7f927a4c
天南十字星 <zjh96@live.cn>
2022-02-17 23:20:15 +0800

    Use related properties in object model unit tests.

--- UnitTests/ComponentModel/TestTypes/ObservableDataObjectTestTypes.cs
+++ UnitTests/ComponentModel/TestTypes/ObservableDataObjectTestTypes.cs
@@ -6,34 +6,30 @@ namespace XstarS.ComponentModel.TestTypes
     {
         public ObservablePersonName() { }
 
+        protected override void InitializeRelatedProperties()
+        {
+            base.InitializeRelatedProperties();
+            this.SetRelatedProperties(nameof(this.FamilyName), nameof(this.FullName));
+            this.SetRelatedProperties(nameof(this.GivenName), nameof(this.FullName));
+            this.SetRelatedProperties(nameof(this.IsEasternStyle), nameof(this.FullName));
+        }
+
         public string? FamilyName
         {
             get => this.GetProperty<string>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.FullName));
-            }
+            set => this.SetProperty(value);
         }
 
         public string? GivenName
         {
             get => this.GetProperty<string>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.FullName));
-            }
+            set => this.SetProperty(value);
         }
 
         public bool IsEasternStyle
         {
             get => this.GetProperty<bool>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.FullName));
-            }
+            set => this.SetProperty(value);
         }
 
         public string FullName => this.IsEasternStyle ?
@@ -44,37 +40,33 @@ namespace XstarS.ComponentModel.TestTypes
     {
         public ObservableBox() { }
 
+        protected override void InitializeRelatedProperties()
+        {
+            base.InitializeRelatedProperties();
+            this.SetRelatedProperties(nameof(this.Length), nameof(this.Size));
+            this.SetRelatedProperties(nameof(this.Width), nameof(this.Size));
+            this.SetRelatedProperties(nameof(this.Height), nameof(this.Size));
+        }
+
         [Range(0, int.MaxValue)]
         public int Length
         {
             get => this.GetProperty<int>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.Size));
-            }
+            set => this.SetProperty(value);
         }
 
         [Range(0, int.MaxValue)]
         public int Width
         {
             get => this.GetProperty<int>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.Size));
-            }
+            set => this.SetProperty(value);
         }
 
         [Range(0, int.MaxValue)]
         public int Height
         {
             get => this.GetProperty<int>();
-            set
-            {
-                this.SetProperty(value);
-                this.NotifyPropertyChanged(nameof(this.Size));
-            }
+            set => this.SetProperty(value);
         }
 
         public int Size => this.Length * this.Width * this.Height;
