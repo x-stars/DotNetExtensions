7f840f7b1cff04852304580a46dfabd2314c91eb
天南十字星 <zjh96@live.cn>
2022-02-19 21:56:08 +0800

    Add enumerator for Range for range-foreach syntax.

---
+++ UnitTests/RangeEnumeratorTest.cs
@@ -0,0 +1,139 @@
+﻿#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XstarS
+{
+    [TestClass]
+    public class RangeEnumeratorTest
+    {
+        [TestMethod]
+        public void ForEachLoop_PositiveRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in 0..100)
+            {
+                sum += index;
+            }
+            var expected = Enumerable.Range(0, 100).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_NegativeRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in ^100..0)
+            {
+                sum += index;
+            }
+            var expected = Enumerable.Range(-100, 100).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_HybridRange_GetExpectedSum()
+        {
+            var sum = 0;
+            foreach (var index in ^100..100)
+            {
+                sum += index;
+            }
+            var expected = Enumerable.Range(-100, 200).Sum();
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void ForEachLoop_InvalidRange_GetZeroSum()
+        {
+            var sum = 0;
+            foreach (var index in 100..^100)
+            {
+                sum += index;
+            }
+            var expected = 0;
+            Assert.AreEqual(expected, sum);
+        }
+
+        [TestMethod]
+        public void Reset_LoopFinished_GetSameSum()
+        {
+            using var etor = (0..100).GetEnumerator();
+            var iSum = 0;
+            while (etor.MoveNext())
+            {
+                iSum += etor.Current;
+            }
+            etor.Reset();
+            var rSum = 0;
+            while (etor.MoveNext())
+            {
+                rSum += etor.Current;
+            }
+            Assert.AreEqual(iSum, rSum);
+        }
+
+        [TestMethod]
+        public void Dispose_LoopNotStart_MoveNextFalse()
+        {
+            using var etor = (0..100).GetEnumerator();
+            Assert.IsTrue(etor.MoveNext());
+            etor.Reset();
+            etor.Dispose();
+            Assert.IsFalse(etor.MoveNext());
+        }
+
+#if NET5_0_OR_GREATER
+        [TestMethod]
+        public void BenchTest_CompareWithForLoop_TimeLessThanTwo()
+        {
+#if DEBUG
+            Assert.IsTrue(true);
+#else
+            const int loopCount = 1000000;
+            Console.WriteLine("Loop count: {0}", loopCount);
+            var sw = new Stopwatch();
+            for (int index = 0; index < loopCount; index++) { }
+            sw.Restart();
+            for (int index = 0; index < loopCount; index++) { }
+            sw.Stop();
+            var forLoopTicks = sw.ElapsedTicks;
+            Console.WriteLine("Legacy for-loop: {0}", sw.Elapsed);
+            sw.Restart();
+            foreach (var index in 0..loopCount) { }
+            sw.Stop();
+            var rangeLoopTicks = sw.ElapsedTicks;
+            Console.WriteLine("Range foreach-loop: {0}", sw.Elapsed);
+            Assert.IsTrue(rangeLoopTicks < forLoopTicks * 2);
+#endif
+        }
+
+        [TestMethod]
+        public void BenchTest_CompareWithEnumerable_TimeLessThanOneQuater()
+        {
+#if DEBUG
+            Assert.IsTrue(true);
+#else
+            const int loopCount = 1000000;
+            Console.WriteLine("Loop count: {0}", loopCount);
+            var sw = new Stopwatch();
+            for (int index = 0; index < loopCount; index++) { }
+            sw.Restart();
+            foreach (var index in Enumerable.Range(0, loopCount)) { }
+            sw.Stop();
+            var forLoopTicks = sw.ElapsedTicks;
+            Console.WriteLine("Enumerable foreach-loop: {0}", sw.Elapsed);
+            sw.Restart();
+            foreach (var index in 0..loopCount) { }
+            sw.Stop();
+            var rangeLoopTicks = sw.ElapsedTicks;
+            Console.WriteLine("Range foreach-loop: {0}", sw.Elapsed);
+            Assert.IsTrue(rangeLoopTicks < forLoopTicks / 4);
+#endif
+        }
+#endif
+    }
+}
+#endif
---
+++ XstarS.Core/RangeEnumerator.cs
@@ -0,0 +1,102 @@
+﻿#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace XstarS
+{
+    using IndexPair = ValueTuple<int, int>;
+
+    /// <summary>
+    /// 为 <see cref="Range"/> 提供创建枚举器的扩展方法。
+    /// </summary>
+    public static class RangeEnumerable
+    {
+        /// <summary>
+        /// 返回用于枚举当前 <see cref="Range"/> 中索引值的枚举器。
+        /// </summary>
+        /// <param name="range">要逐个枚举索引值的 <see cref="Range"/>。</param>
+        /// <returns>逐个枚举 <paramref name="range"/> 中索引值的枚举器。</returns>
+        public static RangeEnumerator GetEnumerator(this Range range)
+        {
+            return new RangeEnumerator(range);
+        }
+    }
+
+    /// <summary>
+    /// 为 <see cref="Range"/> 中的索引值提供枚举器。
+    /// </summary>
+    public struct RangeEnumerator : IEnumerator<int>
+    {
+        /// <summary>
+        /// 表示 <see cref="Range"/> 的包含起始索引值。
+        /// </summary>
+        private readonly int StartIndex;
+
+        /// <summary>
+        /// 表示 <see cref="Range"/> 的不包含结束索引值。
+        /// </summary>
+        private readonly int EndIndex;
+
+        /// <summary>
+        /// 表示枚举器当前位置的索引值。
+        /// </summary>
+        private int CurrentIndex;
+
+        /// <summary>
+        /// 以指定的 <see cref="Range"/> 初始化 <see cref="RangeEnumerator"/> 结构的新实例。
+        /// </summary>
+        /// <param name="range">要逐个枚举索引值的 <see cref="Range"/>。</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe RangeEnumerator(Range range)
+        {
+            var (start, end) = *(IndexPair*)&range;
+            this.StartIndex = (start >= 0) ? start : (start + 1);
+            this.EndIndex = (end >= 0) ? end : (end + 1);
+            this.CurrentIndex = this.StartIndex - 1;
+        }
+
+        /// <summary>
+        /// 获取枚举器当前位置的索引值。
+        /// </summary>
+        /// <returns>枚举器当前位置的索引值。</returns>
+        public int Current => this.CurrentIndex;
+
+        /// <summary>
+        /// 获取枚举器当前位置的索引值。
+        /// </summary>
+        /// <returns>枚举器当前位置的索引值。</returns>
+        object IEnumerator.Current => this.CurrentIndex;
+
+        /// <summary>
+        /// 将枚举器设置为 <see cref="Range"/> 的末尾之后的位置。
+        /// </summary>
+        public void Dispose()
+        {
+            this.CurrentIndex = int.MaxValue - 1;
+        }
+
+        /// <summary>
+        /// 将枚举器推进到下一个索引值。
+        /// </summary>
+        /// <returns>如果枚举器成功推进到下一个索引值，则为 <see langword="true"/>；
+        /// 如果已到达 <see cref="Range"/> 的末尾，则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool MoveNext()
+        {
+            var nextIndex = this.CurrentIndex + 1;
+            if (nextIndex >= this.EndIndex) { return false; }
+            else { this.CurrentIndex = nextIndex; return true;}
+        }
+
+        /// <summary>
+        /// 将枚举器设置为其初始位置，该位置位于第一个索引值之前。
+        /// </summary>
+        public void Reset()
+        {
+            this.CurrentIndex = this.StartIndex - 1;
+        }
+    }
+}
+#endif
