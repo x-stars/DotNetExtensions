c7aa619504b63e7ef2f8b6544cc27ad20abef5ba
天南十字星 <zjh96@live.cn>
2022-02-21 23:17:12 +0800

    Aggressive inline array binary equality methods.

--- XstarS.Core/Runtime/CompilerServices/UnmanagedTypeArray.cs
+++ XstarS.Core/Runtime/CompilerServices/UnmanagedTypeArray.cs
@@ -1,9 +1,10 @@
-﻿namespace XstarS.Runtime.CompilerServices
-{
+﻿using System.Runtime.CompilerServices;
 #if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-    using System;
+using System;
 #endif
 
+namespace XstarS.Runtime.CompilerServices
+{
     /// <summary>
     /// 提供非托管类型的数组相关的帮助方法。
     /// </summary>
@@ -17,6 +18,7 @@
         /// <param name="other">要与当前值进行比较的非托管类型的数组。</param>
         /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool BinaryEquals<T>(this T[]? array, T[]? other) where T : unmanaged
         {
             if (object.ReferenceEquals(array, other)) { return true; }
@@ -38,6 +40,7 @@
         /// <param name="other">要与当前值进行比较的非托管类型的二维数组。</param>
         /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool BinaryEquals<T>(this T[,]? array, T[,]? other) where T : unmanaged
         {
             if (object.ReferenceEquals(array, other)) { return true; }
@@ -59,6 +62,7 @@
         /// <param name="other">要与当前值进行比较的非托管类型的三维数组。</param>
         /// <returns>若 <paramref name="array"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool BinaryEquals<T>(this T[,,]? array, T[,,]? other) where T : unmanaged
         {
             if (object.ReferenceEquals(array, other)) { return true; }
@@ -81,6 +85,7 @@
         /// <param name="other">要与当前跨度进行比较的非托管类型的只读跨度。</param>
         /// <returns>若 <paramref name="span"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool BinaryEquals<T>(this Span<T> span, ReadOnlySpan<T> other)
             where T : unmanaged
         {
@@ -102,6 +107,7 @@
         /// <param name="other">要与当前跨度进行比较的非托管类型的只读跨度。</param>
         /// <returns>若 <paramref name="span"/> 与 <paramref name="other"/> 的值二进制相等，
         /// 则为 <see langword="true"/>；否则为 <see langword="false"/>。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool BinaryEquals<T>(this ReadOnlySpan<T> span, ReadOnlySpan<T> other)
             where T : unmanaged
         {
@@ -122,6 +128,7 @@
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要获取基于二进制值的哈希代码的非托管类型的数组。</param>
         /// <returns><paramref name="array"/> 基于二进制值的哈希代码。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetBinaryHashCode<T>(this T[]? array) where T : unmanaged
         {
             if (array is null) { return 0; }
@@ -139,6 +146,7 @@
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要获取基于二进制值的哈希代码的非托管类型的二维数组。</param>
         /// <returns><paramref name="array"/> 基于二进制值的哈希代码。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetBinaryHashCode<T>(this T[,]? array) where T : unmanaged
         {
             if (array is null) { return 0; }
@@ -156,6 +164,7 @@
         /// <typeparam name="T">非托管类型的数组中的元素的类型。</typeparam>
         /// <param name="array">要获取基于二进制值的哈希代码的非托管类型的三维数组。</param>
         /// <returns><paramref name="array"/> 基于二进制值的哈希代码。</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetBinaryHashCode<T>(this T[,,]? array) where T : unmanaged
         {
             if (array is null) { return 0; }
