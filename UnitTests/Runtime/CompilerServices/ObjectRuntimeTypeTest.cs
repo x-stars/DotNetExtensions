ffce29751916192db70806fbc57441737b3801f8
天南十字星 <zjh96@live.cn>
2022-04-17 01:46:20 +0800

    Add object runtime type extensions and tests.

---
+++ UnitTests/Runtime/CompilerServices/ObjectRuntimeTypeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XstarS.Runtime.CompilerServices
+{
+    [TestClass]
+    public class ObjectRuntimeTypeTest
+    {
+        [TestMethod]
+        public void RefTypeHandle_GetTypeHandle_ValueEquals()
+        {
+            foreach (var value in new[] { 0, 0.0, "", new object() })
+            {
+                var expected = value.GetType().TypeHandle.Value;
+                Assert.AreEqual(expected, value.RefTypeHandle());
+            }
+        }
+
+        [TestMethod]
+        public void UncheckedCast_CompatibleDelegate_CanInvoke()
+        {
+            var negFunc = (Func<int, bool>)(num => num < 0);
+            var negPredicate = negFunc.UncheckedCast<Predicate<int>>();
+            foreach (var number in Enumerable.Range(-8, 16))
+            {
+                Assert.AreEqual(negFunc(number), negPredicate(number));
+            }
+        }
+    }
+}
---
+++ XstarS.Core/Runtime/CompilerServices/ObjectRuntimeType.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace XstarS.Runtime.CompilerServices
+{
+    /// <summary>
+    /// 提供对象运行时类型相关的帮助方法。
+    /// </summary>
+    public static class ObjectRuntimeType
+    {
+        /// <summary>
+        /// 获取当前对象对象的类型句柄 <see cref="IntPtr"/> 的引用。
+        /// </summary>
+        /// <param name="instance">要获取类型句柄引用的对象。</param>
+        /// <returns>对 <paramref name="instance"/> 的类型句柄的引用。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="instance"/> 为 <see langword="null"/>。</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ref IntPtr RefTypeHandle(this object instance)
+        {
+            if (instance is null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+
+            return ref **(IntPtr**)Unsafe.AsPointer(ref instance);
+        }
+
+        /// <summary>
+        /// 创建当前对象的浅表副本，并通过替换类型句柄将其强制转换为指定类型。
+        /// </summary>
+        /// <typeparam name="T">要强制转换为的类型。</typeparam>
+        /// <param name="instance">要强制转换为另一类型的对象。</param>
+        /// <returns>已转换为 <typeparamref name="T"/> 类型的
+        /// <paramref name="instance"/> 的浅表副本。</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="instance"/> 为 <see langword="null"/>。</exception>
+        public static T UncheckedCast<T>(this object instance)
+        {
+            if (instance is null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+
+            var casting = instance.MemberwiseClone();
+            casting.RefTypeHandle() = typeof(T).TypeHandle.Value;
+            return Unsafe.As<object, T>(ref casting);
+        }
+    }
+}
