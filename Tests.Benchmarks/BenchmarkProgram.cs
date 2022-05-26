90e26debe56d7459216385aac8b3791c58cb0bc7
天南十字星 <zjh96@live.cn>
2022-05-26 22:39:09 +0800

    Add R plot exporter to benchmark config.

--- Tests.Benchmarks/BenchmarkProgram.cs
+++ Tests.Benchmarks/BenchmarkProgram.cs
@@ -1,13 +1,21 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Running;
 
-namespace XstarS
+namespace XstarS.GuidGenerators
 {
     internal static class BenchmarkProgram
     {
         internal static void Main(string[] args)
         {
             var assembly = typeof(BenchmarkProgram).Assembly;
-            var summaries = BenchmarkRunner.Run(assembly);
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            if (Environment.GetEnvironmentVariable("R_HOME") != null)
+            {
+                config.AddExporter(RPlotExporter.Default);
+            }
+            var summaries = BenchmarkRunner.Run(assembly, config);
         }
     }
 }
