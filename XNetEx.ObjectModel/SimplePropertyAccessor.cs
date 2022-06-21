e086844578ad45231d38702fe28cc3a1d93d2133
天南十字星 <zjh96@live.cn>
2022-06-21 22:29:16 +0800

    Remove emit dependency of object model.

--- XNetEx.ObjectModel/SimplePropertyAccessor.cs
+++ XNetEx.ObjectModel/SimplePropertyAccessor.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Reflection;
 
 namespace XNetEx.Reflection;
-
-using MethodDynamicDelegate = Func<object?, object?[]?, object?>;
 
 /// <summary>
 /// 提供通过反射访问对象的公共属性的方法。
@@ -12,16 +11,16 @@ using MethodDynamicDelegate = Func<object?, object?[]?, object?>;
 internal static class SimplePropertyAccessor<T> where T : class
 {
     /// <summary>
-    /// 表示当前类型的所有公共实例属性的 <see langword="get"/> 访问器的动态调用委托。
+    /// 表示当前类型的所有公共实例属性的 <see langword="get"/> 访问器方法的元数据对象。
     /// </summary>
-    private static readonly ConcurrentDictionary<string, MethodDynamicDelegate> GetDelegates =
-        new ConcurrentDictionary<string, MethodDynamicDelegate>();
+    private static readonly ConcurrentDictionary<string, MethodInfo> GetMethods =
+        new ConcurrentDictionary<string, MethodInfo>();
 
     /// <summary>
-    /// 表示当前类型的所有公共实例属性的 <see langword="set"/> 访问器的动态调用委托。
+    /// 表示当前类型的所有公共实例属性的 <see langword="set"/> 访问器方法的元数据对象。
     /// </summary>
-    private static readonly ConcurrentDictionary<string, MethodDynamicDelegate> SetDelegates =
-        new ConcurrentDictionary<string, MethodDynamicDelegate>();
+    private static readonly ConcurrentDictionary<string, MethodInfo> SetMethods =
+        new ConcurrentDictionary<string, MethodInfo>();
 
     /// <summary>
     /// 获取指定对象的指定公共属性的值。
@@ -34,9 +33,9 @@ internal static class SimplePropertyAccessor<T> where T : class
     /// 无法找到名为 <paramref name="propertyName"/> 的 <see langword="get"/> 属性。</exception>
     public static object? GetValue(T instance, string propertyName)
     {
-        var getDelegate = SimplePropertyAccessor<T>.GetDelegates.GetOrAdd(
-            propertyName, SimplePropertyAccessor<T>.CreateGetDelegate);
-        return getDelegate.Invoke(instance, Array.Empty<object?>());
+        var getMethod = SimplePropertyAccessor<T>.GetMethods.GetOrAdd(
+            propertyName, SimplePropertyAccessor<T>.FindGetMethod);
+        return getMethod.Invoke(instance, Array.Empty<object?>());
     }
 
     /// <summary>
@@ -51,42 +50,42 @@ internal static class SimplePropertyAccessor<T> where T : class
     /// 无法找到名为 <paramref name="propertyName"/> 的 <see langword="set"/> 属性。</exception>
     public static void SetValue(T instance, string propertyName, object? value)
     {
-        var setDelegate = SimplePropertyAccessor<T>.SetDelegates.GetOrAdd(
-            propertyName, SimplePropertyAccessor<T>.CreateSetDelegate);
-        _ = setDelegate.Invoke(instance, new object?[] { value });
+        var setMethod = SimplePropertyAccessor<T>.SetMethods.GetOrAdd(
+            propertyName, SimplePropertyAccessor<T>.FindSetMethod);
+        _ = setMethod.Invoke(instance, new object?[] { value });
     }
 
     /// <summary>
-    /// 创建当前类型的指定公共实例属性的 <see langword="get"/> 访问器的动态调用委托。
+    /// 查找当前类型的指定公共实例属性的 <see langword="get"/> 访问器方法的元数据对象。
     /// </summary>
-    /// <param name="propertyName">要创建委托的属性的名称。</param>
+    /// <param name="propertyName">要查找访问器方法的属性的名称。</param>
     /// <returns><typeparamref name="T"/> 类型中名为 <paramref name="propertyName"/>
-    /// 的属性的 <see langword="get"/> 访问器的动态调用委托。</returns>
+    /// 的属性的 <see langword="get"/> 访问器方法的元数据对象。</returns>
     /// <exception cref="MissingMemberException">
     /// 无法找到名为 <paramref name="propertyName"/> 的 <see langword="get"/> 属性。</exception>
-    private static MethodDynamicDelegate CreateGetDelegate(string propertyName)
+    private static MethodInfo FindGetMethod(string propertyName)
     {
         var property = typeof(T).GetProperty(propertyName, Type.EmptyTypes);
         if (property?.GetMethod is null) { throw new MissingMemberException(); }
         if (property.GetMethod.IsStatic) { throw new MissingMemberException(); }
         if (!property.GetMethod.IsPublic) { throw new MissingMemberException(); }
-        return property.GetMethod.CreateDynamicDelegate();
+        return property.GetMethod;
     }
 
     /// <summary>
-    /// 创建当前类型的指定公共实例属性的 <see langword="set"/> 访问器的动态调用委托。
+    /// 查找当前类型的指定公共实例属性的 <see langword="set"/> 访问器方法的元数据对象。
     /// </summary>
-    /// <param name="propertyName">要创建委托的属性的名称。</param>
+    /// <param name="propertyName">要查找访问器方法的属性的名称。</param>
     /// <returns><typeparamref name="T"/> 类型中名为 <paramref name="propertyName"/>
-    /// 的属性的 <see langword="set"/> 访问器的动态调用委托。</returns>
+    /// 的属性的 <see langword="set"/> 访问器方法的元数据对象。</returns>
     /// <exception cref="MissingMemberException">
     /// 无法访问名为 <paramref name="propertyName"/> 的 <see langword="set"/> 属性。</exception>
-    private static MethodDynamicDelegate CreateSetDelegate(string propertyName)
+    private static MethodInfo FindSetMethod(string propertyName)
     {
         var property = typeof(T).GetProperty(propertyName, Type.EmptyTypes);
         if (property?.SetMethod is null) { throw new MissingMemberException(); }
         if (property.SetMethod.IsStatic) { throw new MissingMemberException(); }
         if (!property.SetMethod.IsPublic) { throw new MissingMemberException(); }
-        return property.SetMethod.CreateDynamicDelegate();
+        return property.SetMethod;
     }
 }
