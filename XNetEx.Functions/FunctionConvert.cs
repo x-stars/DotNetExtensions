57de91ffc7ea39a9ba8e68053b7be31a22a665e8
天南十字星 <zjh96@live.cn>
2023-06-05 00:12:06 +0800

    Make function conversion method as extensions.

--- XNetEx.Functions/FunctionConvert.cs
+++ XNetEx.Functions/FunctionConvert.cs
@@ -5,22 +5,22 @@ namespace XNetEx.Functions;
 
 public static class FunctionConvert
 {
-    public static Action<object?, EventArgs> ToAction(EventHandler handler) =>
-        FunctionConvert.CloneAndChangeType<Action<object?, EventArgs>>(handler);
+    public static Action<object?, EventArgs> ToAction(this EventHandler handler) =>
+        handler.CloneAndChangeType<Action<object?, EventArgs>>();
 
-    public static Action<object?, TArgs> ToAction<TArgs>(EventHandler<TArgs> handler) =>
-        FunctionConvert.CloneAndChangeType<Action<object?, TArgs>>(handler);
+    public static Action<object?, TArgs> ToAction<TArgs>(this EventHandler<TArgs> handler) =>
+        handler.CloneAndChangeType<Action<object?, TArgs>>();
 
-    public static Func<T, T, int> ToFunc<T>(Comparison<T> comparison) =>
-        FunctionConvert.CloneAndChangeType<Func<T, T, int>>(comparison);
+    public static Func<T, T, int> ToFunc<T>(this Comparison<T> comparison) =>
+        comparison.CloneAndChangeType<Func<T, T, int>>();
 
-    public static Func<TIn, TOut> ToFunc<TIn, TOut>(Converter<TIn, TOut> converter) =>
-        FunctionConvert.CloneAndChangeType<Func<TIn, TOut>>(converter);
+    public static Func<TIn, TOut> ToFunc<TIn, TOut>(this Converter<TIn, TOut> converter) =>
+        converter.CloneAndChangeType<Func<TIn, TOut>>();
 
-    public static Func<T, bool> ToFunc<T>(Predicate<T> predicate) =>
-        FunctionConvert.CloneAndChangeType<Func<T, bool>>(predicate);
+    public static Func<T, bool> ToFunc<T>(this Predicate<T> predicate) =>
+        predicate.CloneAndChangeType<Func<T, bool>>();
 
-    private static TDelegate CloneAndChangeType<TDelegate>(Delegate instance)
+    private static TDelegate CloneAndChangeType<TDelegate>(this Delegate instance)
         where TDelegate : Delegate
     {
         var copy = RuntimeObject.MemberwiseClone(instance);
