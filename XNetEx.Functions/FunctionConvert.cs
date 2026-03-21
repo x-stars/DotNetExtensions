a8f07c0809c0bef2ef6228524a79e77eb1d1ebf1
天南十字星 <zjh96@live.cn>
2026-03-21 20:09:58 +0800

    Avoid using tricky type cast for functions.

--- XNetEx.Functions/FunctionConvert.cs
+++ XNetEx.Functions/FunctionConvert.cs
@@ -1,30 +1,16 @@
 ﻿using System;
-using XNetEx.Runtime.CompilerServices;
 
 namespace XNetEx.Functions;
 
 public static class FunctionConvert
 {
-    public static Action<object?, EventArgs> ToAction(this EventHandler handler) =>
-        handler.CloneAndChangeType<Action<object?, EventArgs>>();
+    public static Action<object?, EventArgs> ToAction(this EventHandler handler) => handler.Invoke;
 
-    public static Action<object?, TArgs> ToAction<TArgs>(this EventHandler<TArgs> handler) =>
-        handler.CloneAndChangeType<Action<object?, TArgs>>();
+    public static Action<object?, TArgs> ToAction<TArgs>(this EventHandler<TArgs> handler) => handler.Invoke;
 
-    public static Func<T, T, int> ToFunc<T>(this Comparison<T> comparison) =>
-        comparison.CloneAndChangeType<Func<T, T, int>>();
+    public static Func<T, T, int> ToFunc<T>(this Comparison<T> comparison) => comparison.Invoke;
 
-    public static Func<TIn, TOut> ToFunc<TIn, TOut>(this Converter<TIn, TOut> converter) =>
-        converter.CloneAndChangeType<Func<TIn, TOut>>();
+    public static Func<TIn, TOut> ToFunc<TIn, TOut>(this Converter<TIn, TOut> converter) => converter.Invoke;
 
-    public static Func<T, bool> ToFunc<T>(this Predicate<T> predicate) =>
-        predicate.CloneAndChangeType<Func<T, bool>>();
-
-    private static TDelegate CloneAndChangeType<TDelegate>(this Delegate instance)
-        where TDelegate : Delegate
-    {
-        var copy = RuntimeObject.MemberwiseClone(instance);
-        RuntimeObject.TypeHandleOf(copy) = typeof(TDelegate).TypeHandle.Value;
-        return (TDelegate)copy;
-    }
+    public static Func<T, bool> ToFunc<T>(this Predicate<T> predicate) => predicate.Invoke;
 }
